fe9ad355b147ee3198ce20b718d193eac2af678e
msutton <mikisiton2@gmail.com>
2022-08-14 20:17:56 +0300

    Cleanup of pipeline demo

--- core/src/instruction.rs
+++
@@ -1,6 +0,0 @@
-#[derive(Debug)]
-pub enum Instruction {
-    Shutdown,
-    TestInstructionForService(u64),
-    TestInstructionForConsumer(u64),
-}
--- core/src/lib.rs
+++ core/src/lib.rs
@@ -2,11 +2,6 @@ extern crate self as kaspa_core;
 
 pub mod console;
 pub mod core;
-pub mod instruction;
 pub mod log;
-pub mod monitor;
 pub mod service;
 pub mod signals;
-pub mod test_consumer;
-pub mod test_emitter;
-pub mod test_service;
