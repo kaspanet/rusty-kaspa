f0782b252491bdc8c7c546f4d6d0e3f80c317dee
msutton <mikisiton2@gmail.com>
2022-09-01 17:17:42 +0300

    Imp utxo collection intersects

--- consensus/core/src/utxo/utxo_collection.rs
+++ consensus/core/src/utxo/utxo_collection.rs
@@ -4,9 +4,18 @@ use std::collections::HashMap;
 pub type UtxoCollection = HashMap<TransactionOutpoint, UtxoEntry>;
 
 pub trait UtxoCollectionExtensions {
+    /// Checks if the `outpoint` key exists with an entry that holds `entry.block_daa_score == daa_score`
     fn contains_with_daa_score(&self, outpoint: &TransactionOutpoint, daa_score: u64) -> bool;
-    fn add_set(&mut self, other: &Self);
-    fn remove_set(&mut self, other: &Self);
+
+    /// Adds all entries from `other` to `self`.
+    /// Note that this means that values from `other` might override values of `self`.   
+    fn add_many(&mut self, other: &Self);
+
+    /// Removes all elements in `other` from `self`. Equivalent to `self - other` in set theory.
+    fn remove_many(&mut self, other: &Self);
+
+    /// Returns whether the intersection between the two collections is not empty.
+    fn intersects(&self, other: &Self) -> bool;
 }
 
 impl UtxoCollectionExtensions for UtxoCollection {
@@ -18,16 +27,28 @@ impl UtxoCollectionExtensions for UtxoCollection {
         }
     }
 
-    fn add_set(&mut self, other: &Self) {
+    fn add_many(&mut self, other: &Self) {
         for (k, v) in other.iter() {
             self.insert(*k, v.clone());
         }
     }
 
-    fn remove_set(&mut self, other: &Self) {
-        for (k, _) in other.iter() {
+    fn remove_many(&mut self, other: &Self) {
+        for k in other.keys() {
             self.remove(k);
         }
+    }
+
+    fn intersects(&self, other: &Self) -> bool {
+        // We prefer iterating over the smaller set
+        let (keys, other) = if self.len() <= other.len() { (self.keys(), other) } else { (other.keys(), self) };
+
+        for k in keys {
+            if other.contains_key(k) {
+                return true;
+            }
+        }
+        false
     }
 }
 
