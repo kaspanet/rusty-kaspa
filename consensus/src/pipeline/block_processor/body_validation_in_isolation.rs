3e3066429fba26180752172421d6b3a3948f21d1
Ori Newman <orinewman1@gmail.com>
2022-09-07 13:19:06 +0300

    Add file

---
+++ consensus/src/pipeline/block_processor/body_validation_in_isolation.rs
@@ -0,0 +1,21 @@
+use consensus_core::{block::Block, hashing};
+use misc::merkle::calc_hash_merkle_root;
+
+use crate::errors::{BlockProcessResult, RuleError};
+
+use super::BlockBodyProcessor;
+
+impl BlockBodyProcessor {
+    pub fn validate_body_in_isolation(block: &Block) -> BlockProcessResult<()> {
+        Self::check_hash_merkle_tree(block)
+    }
+
+    fn check_hash_merkle_tree(block: &Block) -> BlockProcessResult<()> {
+        let tx_hashes = block.transactions.iter().map(hashing::tx::hash);
+        let calculated = calc_hash_merkle_root(block.transactions.iter());
+        if calculated != block.header.hash_merkle_root {
+            return Err(RuleError::BadMerkleRoot(block.header.hash_merkle_root, calculated));
+        }
+        Ok(())
+    }
+}
