b30f13fc8bae8daaf3c130f17c3139c6cb51fe95
Elichai Turkel <elichai.turkel@gmail.com>
2022-09-11 20:39:04 +0300

    Change fuzzing corpuses directory to rusty-kaspa-corpuses

--- math/fuzz/fuzz.sh
+++ math/fuzz/fuzz.sh
@@ -3,4 +3,4 @@ rustc --version
 cargo install cargo-fuzz
 fuzzer="$1"
 shift;
-cargo fuzz run "$fuzzer" --release -- -use_counters=1 -use_value_profile=1 "$@"
+cargo fuzz run "$fuzzer" --release -- -use_counters=1 -use_value_profile=1 "$@" ../../../rusty-kaspa-corpus/math/"$fuzzer"
