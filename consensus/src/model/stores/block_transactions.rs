96008df04abc27e5da6115cec7e8e69fbf6b169d
Ori Newman <orinewman1@gmail.com>
2022-09-14 16:30:21 +0300

    Simplify store code

--- consensus/src/model/stores/block_transactions.rs
+++ consensus/src/model/stores/block_transactions.rs
@@ -30,10 +30,7 @@ impl DbBlockTransactionsStore {
     }
 
     pub fn clone_with_new_cache(&self, cache_size: u64) -> Self {
-        Self {
-            raw_db: Arc::clone(&self.raw_db),
-            cached_access: CachedDbAccess::new(Arc::clone(&self.raw_db), cache_size, STORE_PREFIX),
-        }
+        Self::new(Arc::clone(&self.raw_db), cache_size)
     }
 
     pub fn insert_batch(
