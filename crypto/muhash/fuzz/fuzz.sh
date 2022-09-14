7c2452c6a050bcd5eb23f764db6cc7a3273b67c8
Elichai Turkel <elichai.turkel@gmail.com>
2022-09-14 11:54:02 +0300

    Add Uint implementation (#39)
    * Start implementing a stack based big int
    
    * Uint: Fix shift left/right
    
    * Uint: Formatting
    
    * Uint add Division and Reminder
    
    * Uint: Cleanup
    
    * Uint: add serde support and cleanup
    
    * Rename misc to math, and add more impls to Uint
    
    * Uint: Add benchmarks
    
    * Uint: Add U320 and add missing impls
    
    * Replace BigInt with U256/U320
    
    * Uint: Add a u128 fuzzer
    
    * Uint: Add a Uint256 fuzzer
    
    * Fix addition overflow bug
    
    * Change fuzzing corpuses directory to rusty-kaspa-corpuses

--- crypto/muhash/fuzz/fuzz.sh
+++ crypto/muhash/fuzz/fuzz.sh
@@ -2,4 +2,4 @@
 rustc --version
 cargo install cargo-fuzz
 
-cargo fuzz run u3072 --debug-assertions --release -- -use_counters=1 -use_value_profile=1 "$@"
+cargo fuzz run u3072 --debug-assertions --release -- -use_counters=1 -use_value_profile=1 "$@" ../../../../rusty-kaspa-corpus/muhash/u3072/
