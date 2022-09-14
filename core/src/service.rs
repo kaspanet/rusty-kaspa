e12849dcd3b29f1bd8fd65c5ef0a556f235b46b1
Elichai Turkel <elichai.turkel@gmail.com>
2022-09-14 15:01:41 +0300

    Change ConsensusMonitor::ident to static str

--- core/src/service.rs
+++ core/src/service.rs
@@ -3,7 +3,7 @@ use intertrait::CastFromSync;
 use std::{sync::Arc, thread::JoinHandle};
 
 pub trait Service: CastFromSync {
-    fn ident(self: Arc<Self>) -> String;
+    fn ident(self: Arc<Self>) -> &'static str;
     fn start(self: Arc<Self>, core: Arc<Core>) -> Vec<JoinHandle<()>>;
     fn stop(self: Arc<Self>);
 }
