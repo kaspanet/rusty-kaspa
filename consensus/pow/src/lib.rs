3f0cf21b1e1c2094051403c21e6316ccc2ffe9dd
Elichai Turkel <elichai.turkel@gmail.com>
2022-09-29 20:59:13 +0300

    Add a pow crate to consensus

---
+++ consensus/pow/src/lib.rs
@@ -0,0 +1,49 @@
+// public for benchmarks
+#[doc(hidden)]
+pub mod matrix;
+#[doc(hidden)]
+pub mod xoshiro;
+
+use crate::matrix::Matrix;
+use consensus_core::header::Header;
+use hashes::PowHash;
+use math::Uint256;
+
+/// State is an intermediate data structure with pre-computed values to speed up mining.
+pub struct State {
+    matrix: Matrix,
+    target: Uint256,
+    // PRE_POW_HASH || TIME || 32 zero byte padding; without NONCE
+    hasher: PowHash,
+}
+
+impl State {
+    #[inline(always)]
+    pub fn new(header: &Header) -> Self {
+        let target = Uint256::from_compact_target_bits(header.bits);
+        let pre_pow_hash = header.hash;
+        // PRE_POW_HASH || TIME || 32 zero byte padding || NONCE
+        let hasher = PowHash::new(pre_pow_hash, header.timestamp);
+        let matrix = Matrix::generate(pre_pow_hash);
+
+        Self { matrix, target, hasher }
+    }
+
+    #[inline(always)]
+    #[must_use]
+    /// PRE_POW_HASH || TIME || 32 zero byte padding || NONCE
+    pub fn calculate_pow(&self, nonce: u64) -> Uint256 {
+        // Hasher already contains PRE_POW_HASH || TIME || 32 zero byte padding; so only the NONCE is missing
+        let hash = self.hasher.clone().finalize_with_nonce(nonce);
+        let hash = self.matrix.heavy_hash(hash);
+        Uint256::from_le_bytes(hash.as_bytes())
+    }
+
+    #[inline(always)]
+    #[must_use]
+    pub fn check_pow(&self, nonce: u64) -> bool {
+        let pow = self.calculate_pow(nonce);
+        // The pow hash must be less or equal than the claimed target.
+        pow <= self.target
+    }
+}
