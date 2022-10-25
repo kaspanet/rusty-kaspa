a9aca72d3cfa83a940543eeff4999f6617042a07
msutton <mikisiton2@gmail.com>
2022-10-25 12:28:11 +0300

    Fix key parsing

--- consensus/src/model/stores/database/key.rs
+++ consensus/src/model/stores/database/key.rs
@@ -5,15 +5,19 @@ const SEP: u8 = b'/';
 #[derive(Debug, Clone)]
 pub struct DbKey {
     path: Vec<u8>,
+    prefix_len: usize,
 }
 
 impl DbKey {
     pub fn new<TKey: Copy + AsRef<[u8]>>(prefix: &[u8], key: TKey) -> Self {
-        Self { path: prefix.iter().chain(std::iter::once(&SEP)).chain(key.as_ref().iter()).copied().collect() }
+        Self {
+            path: prefix.iter().chain(std::iter::once(&SEP)).chain(key.as_ref().iter()).copied().collect(),
+            prefix_len: prefix.len() + 1, // Include `SEP` as part of the prefix
+        }
     }
 
     pub fn prefix_only(prefix: &[u8]) -> Self {
-        Self::new(prefix, b"")
+        Self::new(prefix, &[])
     }
 }
 
@@ -25,10 +29,16 @@ impl AsRef<[u8]> for DbKey {
 
 impl Display for DbKey {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        let pos = self.path.len() - 1 - self.path.iter().rev().position(|c| *c == SEP).unwrap(); // Find the last position of `SEP`
-        let (prefix, key) = (&self.path[..pos], &self.path[pos + 1..]);
-        f.write_str(str::from_utf8(prefix).unwrap_or("{cannot display prefix}"))?;
-        f.write_str("/")?;
+        let (prefix, key) = self.path.split_at(self.prefix_len);
+        // We expect the prefix to be human readable
+        if let Ok(s) = str::from_utf8(prefix) {
+            f.write_str(s)?;
+        } else {
+            // Otherwise we fallback to hex parsing
+            f.write_str(&faster_hex::hex_string(&prefix[..prefix.len() - 1]))?; // Drop `SEP`
+            f.write_str("/")?;
+        }
+        // We expect that key is usually more readable as hex
         f.write_str(&faster_hex::hex_string(key))
     }
 }
@@ -36,17 +46,20 @@ impl Display for DbKey {
 #[cfg(test)]
 mod tests {
     use super::*;
-    use hashes::Hash;
+    use hashes::{Hash, HASH_SIZE};
 
     #[test]
     fn test_key_display() {
         let key1 = DbKey::new(b"human-readable", Hash::from_u64_word(34567890));
-        let key2 = DbKey::new(&[1, 2, 2, 89], Hash::from_u64_word(345690));
-        let key3 = DbKey::prefix_only(&[1, 2, 2, 89]);
-        let key4 = DbKey::prefix_only(b"direct-prefix");
-        println!("{}", key1);
-        println!("{}", key2);
-        println!("{}", key3);
-        println!("{}", key4);
+        let key2 = DbKey::new(&[0xC0, 0xC1, 0xF5, 0xF6], Hash::from_u64_word(345690)); // `0xC0, 0xC1, 0xF5, 0xF6` are invalid UTF-8 chars
+        let key3 = DbKey::new(b"human/readable", Hash::from_bytes([SEP; HASH_SIZE])); // Add many binary `/` to assert prefix is recognized
+        let key4 = DbKey::prefix_only(&[0xC0, 0xC1, 0xF5, 0xF6]);
+        let key5 = DbKey::prefix_only(b"direct-prefix");
+
+        assert!(key1.to_string().starts_with("human-readable/"));
+        assert!(key2.to_string().starts_with("c0c1f5f6/")); // Expecting hex since invalid UTF-8 was used
+        assert!(key3.to_string().starts_with("human/readable/"));
+        assert_eq!(key4.to_string(), "c0c1f5f6/");
+        assert_eq!(key5.to_string(), "direct-prefix/");
     }
 }
