fd42e13384b4d29c950935e1d6d186ce51143d03
msutton <mikisiton2@gmail.com>
2022-11-09 21:46:04 +0200

    Move to sim::infra

--- simpa/src/main.rs
+++ simpa/src/main.rs
@@ -1,13 +1,14 @@
 #![feature(generators, generator_trait)]
 
 use rand_distr::{Distribution, Poisson};
-use std::cell::RefCell;
-use std::collections::{BinaryHeap, HashMap};
+use simulator::infra::{Env, Yield};
+use std::collections::HashMap;
 use std::ops::Generator;
-use std::ops::GeneratorState::Yielded;
-use std::pin::Pin;
 use std::rc::Rc;
 
+pub mod simulator;
+
+#[derive(Default)]
 struct Message {
     msg: String,
 }
@@ -15,121 +16,6 @@ struct Message {
 impl Message {
     fn new(msg: String) -> Self {
         Self { msg }
-    }
-}
-
-struct SimulationEvent {
-    timestamp: u64,
-    dest: u64,
-    msg: Option<Message>,
-}
-
-impl SimulationEvent {
-    fn new(timestamp: u64, dest: u64, msg: Option<Message>) -> Self {
-        Self { timestamp, dest, msg }
-    }
-}
-
-impl PartialEq for SimulationEvent {
-    fn eq(&self, other: &Self) -> bool {
-        self.timestamp == other.timestamp && self.dest == other.dest
-    }
-}
-
-impl Eq for SimulationEvent {}
-
-impl PartialOrd for SimulationEvent {
-    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
-        Some(self.cmp(other))
-    }
-}
-
-impl Ord for SimulationEvent {
-    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
-        // Reversing so that min timestamp is scheduled first
-        other.timestamp.cmp(&self.timestamp).then_with(|| other.dest.cmp(&self.dest))
-    }
-}
-
-pub enum SimulationYield {
-    Timeout(u64),
-    Receive,
-}
-
-pub type Process = Box<dyn Unpin + Generator<Yield = SimulationYield, Return = ()>>;
-
-#[derive(Default)]
-struct InnerEnv {
-    time: u64,
-    scheduler: BinaryHeap<SimulationEvent>,
-    inboxes: HashMap<u64, Message>,
-}
-
-impl InnerEnv {
-    fn send(&mut self, delay: u64, dest: u64, msg: Message) {
-        self.scheduler.push(SimulationEvent::new(self.time + delay, dest, Some(msg)))
-    }
-
-    fn timeout(&mut self, timeout: u64, dest: u64) {
-        self.scheduler.push(SimulationEvent::new(self.time + timeout, dest, None))
-    }
-
-    fn next(&mut self) -> u64 {
-        let event = self.scheduler.pop().unwrap();
-        self.time = event.timestamp;
-        if let Some(msg) = event.msg {
-            self.inboxes.insert(event.dest, msg);
-        }
-        event.dest
-    }
-
-    fn inbox(&mut self, dest: u64) -> Message {
-        self.inboxes.remove(&dest).unwrap()
-    }
-}
-
-#[derive(Default)]
-struct Env {
-    inner: RefCell<InnerEnv>,
-}
-
-impl Env {
-    fn send(&self, delay: u64, dest: u64, msg: Message) {
-        self.inner.borrow_mut().send(delay, dest, msg)
-    }
-
-    fn timeout(&self, timeout: u64, dest: u64) {
-        self.inner.borrow_mut().timeout(timeout, dest)
-    }
-
-    pub fn next(&self) -> u64 {
-        self.inner.borrow_mut().next()
-    }
-
-    pub fn run(&self, processes: &mut HashMap<u64, Process>, until: u64) {
-        for i in processes.keys().copied().collect::<Vec<u64>>() {
-            let Yielded(sim_yield) = Pin::new(processes.get_mut(&i).unwrap().as_mut()).resume(()) else { unreachable!() };
-            match sim_yield {
-                SimulationYield::Timeout(timeout) => self.timeout(timeout, i),
-                SimulationYield::Receive => {}
-            }
-        }
-
-        loop {
-            let dest = self.next();
-            let Yielded(sim_yield) = Pin::new(processes.get_mut(&dest).unwrap().as_mut()).resume(()) else { unreachable!() };
-            match sim_yield {
-                SimulationYield::Timeout(timeout) => self.timeout(timeout, dest),
-                SimulationYield::Receive => {}
-            }
-            if self.inner.borrow().time > until {
-                break;
-            }
-        }
-    }
-
-    pub fn inbox(&self, dest: u64) -> Message {
-        self.inner.borrow_mut().inbox(dest)
     }
 }
 
@@ -142,7 +28,7 @@ impl Sender {
         Self { id }
     }
 
-    pub fn send(self, env: Rc<Env>) -> impl Generator<Yield = SimulationYield, Return = ()> {
+    pub fn send(self, env: Rc<Env<Message>>) -> impl Generator<Yield = Yield, Return = ()> {
         move || {
             let poi = Poisson::new(8f64).unwrap();
             let mut thread_rng = rand::thread_rng();
@@ -153,8 +39,7 @@ impl Sender {
                 let delay = 0;
                 env.send(delay, 0, msg);
 
-                let timeout = poi.sample(&mut thread_rng) as u64;
-                yield SimulationYield::Timeout(timeout);
+                yield Yield::Timeout(poi.sample(&mut thread_rng) as u64);
             }
         }
     }
@@ -169,18 +54,18 @@ impl Receiver {
         Self { id }
     }
 
-    pub fn receive(self, env: Rc<Env>) -> impl Generator<Yield = SimulationYield, Return = ()> {
+    pub fn receive(self, env: Rc<Env<Message>>) -> impl Generator<Yield = Yield, Return = ()> {
         move || loop {
-            yield SimulationYield::Receive;
+            yield Yield::Wait;
             println!("{}", env.inbox(self.id).msg);
         }
     }
 }
 
 fn main() {
-    let env = Rc::new(Env::default());
+    let env = Rc::new(Env::<Message>::default());
 
-    let mut processes = HashMap::<u64, Box<dyn Unpin + Generator<Yield = SimulationYield, Return = ()>>>::new();
+    let mut processes = HashMap::<u64, Box<dyn Unpin + Generator<Yield = Yield, Return = ()>>>::new();
     processes.insert(0, Box::new(Receiver::new(0).receive(env.clone())));
     processes.insert(1, Box::new(Sender::new(1).send(env.clone())));
     processes.insert(2, Box::new(Sender::new(2).send(env.clone())));
---
+++ simpa/src/simulator/infra.rs
@@ -0,0 +1,124 @@
+use std::ops::GeneratorState::{Yielded, Complete};
+use std::pin::Pin;
+use std::{
+    cell::RefCell,
+    collections::{BinaryHeap, HashMap},
+    ops::Generator,
+};
+
+pub struct Event<T> {
+    timestamp: u64,
+    dest: u64,
+    msg: Option<T>,
+}
+
+impl<T> Event<T> {
+    pub fn new(timestamp: u64, dest: u64, msg: Option<T>) -> Self {
+        Self { timestamp, dest, msg }
+    }
+}
+
+impl<T> PartialEq for Event<T> {
+    fn eq(&self, other: &Self) -> bool {
+        self.timestamp == other.timestamp && self.dest == other.dest
+    }
+}
+
+impl<T> Eq for Event<T> {}
+
+impl<T> PartialOrd for Event<T> {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+impl<T> Ord for Event<T> {
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        // Reversing so that min timestamp is scheduled first
+        other.timestamp.cmp(&self.timestamp).then_with(|| other.dest.cmp(&self.dest))
+    }
+}
+
+pub enum Yield {
+    Timeout(u64),
+    Wait,
+}
+
+pub type Process = Box<dyn Unpin + Generator<Yield = Yield, Return = ()>>;
+
+#[derive(Default)]
+struct InnerEnv<T> {
+    time: u64,
+    scheduler: BinaryHeap<Event<T>>,
+    inboxes: HashMap<u64, T>,
+}
+
+impl<T> InnerEnv<T> {
+    fn send(&mut self, delay: u64, dest: u64, msg: T) {
+        self.scheduler.push(Event::new(self.time + delay, dest, Some(msg)))
+    }
+
+    fn timeout(&mut self, timeout: u64, dest: u64) {
+        self.scheduler.push(Event::new(self.time + timeout, dest, None))
+    }
+
+    fn next(&mut self) -> u64 {
+        let event = self.scheduler.pop().unwrap();
+        self.time = event.timestamp;
+        if let Some(msg) = event.msg {
+            self.inboxes.insert(event.dest, msg);
+        }
+        event.dest
+    }
+
+    fn inbox(&mut self, dest: u64) -> T {
+        self.inboxes.remove(&dest).unwrap()
+    }
+}
+
+#[derive(Default)]
+pub struct Env<T> {
+    inner: RefCell<InnerEnv<T>>,
+}
+
+impl<T> Env<T> {
+    pub fn send(&self, delay: u64, dest: u64, msg: T) {
+        self.inner.borrow_mut().send(delay, dest, msg)
+    }
+
+    pub fn timeout(&self, timeout: u64, dest: u64) {
+        self.inner.borrow_mut().timeout(timeout, dest)
+    }
+
+    pub fn next(&self) -> u64 {
+        self.inner.borrow_mut().next()
+    }
+
+    pub fn step(&self, id: u64, process: &mut Process) {
+        match Pin::new(process).resume(()) {
+            Yielded(yielded) => match yielded {
+                Yield::Timeout(timeout) => self.timeout(timeout, id),
+                Yield::Wait => {}
+            },
+            Complete(_) => {},
+        }
+    }
+
+    pub fn run(&self, processes: &mut HashMap<u64, Process>, until: u64) {
+        for i in processes.keys().copied().collect::<Vec<u64>>() {
+            self.step(i, processes.get_mut(&i).unwrap());
+        }
+
+        loop {
+            let dest = self.next();
+            self.step(dest, processes.get_mut(&dest).unwrap());
+            if self.inner.borrow().time > until {
+                break;
+            }
+        }
+    }
+
+    pub fn inbox(&self, dest: u64) -> T {
+        self.inner.borrow_mut().inbox(dest)
+    }
+}
