26fe97267bbba04eacc62fb80a58d75c19838d00
msutton <mikisiton2@gmail.com>
2022-11-14 03:29:02 +0200

    Validate with concurrency

--- simpa/src/main.rs
+++ simpa/src/main.rs
@@ -1,5 +1,6 @@
 use consensus::{
     consensus::{test_consensus::create_temp_db, Consensus},
+    errors::{BlockProcessResult, RuleError},
     model::stores::{
         block_transactions::BlockTransactionsStoreReader, headers::HeaderStoreReader, relations::RelationsStoreReader,
         statuses::BlockStatus,
@@ -7,7 +8,9 @@ use consensus::{
     params::{Params, DEVNET_PARAMS},
 };
 use consensus_core::{block::Block, BlockHashSet, HashMapCustomHasher};
+use futures::{future::join_all, Future};
 use hashes::Hash;
+use itertools::Itertools;
 use simulator::network::KaspaNetworkSimulator;
 use std::{collections::VecDeque, sync::Arc};
 
@@ -25,33 +28,58 @@ fn main() {
     let (_lifetime2, db2) = create_temp_db();
     let consensus2 = Arc::new(Consensus::new(db2, &params));
     let handles2 = consensus2.init();
-    let start = std::time::Instant::now();
     validate(&consensus, &consensus2, &params);
-    eprintln!("Validation elapsed {:?}", start.elapsed());
     consensus2.shutdown(handles2);
     drop(consensus);
 }
 
 #[tokio::main]
-async fn validate(consensus: &Arc<Consensus>, consensus2: &Arc<Consensus>, params: &Params) {
-    let hashes = topologically_ordered_hashes(consensus, params);
+async fn validate(src_consensus: &Consensus, dst_consensus: &Consensus, params: &Params) {
+    let hashes = topologically_ordered_hashes(src_consensus, params);
     eprintln!("Validating {} blocks", hashes.len());
-    for (i, &hash) in hashes.iter().enumerate() {
-        let block =
-            Block::from_arcs(consensus.headers_store.get_header(hash).unwrap(), consensus.block_transactions_store.get(hash).unwrap());
-        let status =
-            consensus2.validate_and_insert_block(block).await.unwrap_or_else(|e| panic!("block {} {} failed: {}", i, hash, e));
-        assert!(status.is_utxo_valid_or_pending());
+    let start = std::time::Instant::now();
+    let chunks = hashes.into_iter().chunks(1000);
+    let mut iter = chunks.into_iter();
+    let mut chunk = iter.next().unwrap();
+    let mut prev_joins = submit_chunk(src_consensus, dst_consensus, &mut chunk);
+
+    for mut chunk in iter {
+        let current_joins = submit_chunk(src_consensus, dst_consensus, &mut chunk);
+        let statuses = join_all(prev_joins).await.into_iter().collect::<Result<Vec<BlockStatus>, RuleError>>().unwrap();
+        assert!(statuses.iter().all(|s| s.is_utxo_valid_or_pending()));
+        prev_joins = current_joins;
     }
+
+    let statuses = join_all(prev_joins).await.into_iter().collect::<Result<Vec<BlockStatus>, RuleError>>().unwrap();
+    assert!(statuses.iter().all(|s| s.is_utxo_valid_or_pending()));
+
     // Assert that at least one body tip was resolved with valid UTXO
-    assert!(consensus2.body_tips().iter().copied().any(|h| consensus2.block_status(h) == BlockStatus::StatusUTXOValid));
+    assert!(dst_consensus.body_tips().iter().copied().any(|h| dst_consensus.block_status(h) == BlockStatus::StatusUTXOValid));
+    eprintln!("Validation time: {:?}", start.elapsed());
 }
 
-fn topologically_ordered_hashes(consensus: &Arc<Consensus>, params: &Params) -> Vec<Hash> {
+fn submit_chunk(
+    src_consensus: &Consensus,
+    dst_consensus: &Consensus,
+    chunk: &mut impl Iterator<Item = Hash>,
+) -> Vec<impl Future<Output = BlockProcessResult<BlockStatus>>> {
+    let mut futures = Vec::new();
+    for hash in chunk {
+        let block = Block::from_arcs(
+            src_consensus.headers_store.get_header(hash).unwrap(),
+            src_consensus.block_transactions_store.get(hash).unwrap(),
+        );
+        let f = dst_consensus.validate_and_insert_block(block);
+        futures.push(f);
+    }
+    futures
+}
+
+fn topologically_ordered_hashes(src_consensus: &Consensus, params: &Params) -> Vec<Hash> {
     let mut queue: VecDeque<Hash> = std::iter::once(params.genesis_hash).collect();
     let mut visited = BlockHashSet::new();
     let mut vec = Vec::new();
-    let relations = consensus.relations_store.read();
+    let relations = src_consensus.relations_store.read();
     while let Some(current) = queue.pop_front() {
         for child in relations.get_children(current).unwrap().iter() {
             if visited.insert(*child) {
@@ -60,6 +88,6 @@ fn topologically_ordered_hashes(consensus: &Arc<Consensus>, params: &Params) -> 
             }
         }
     }
-    vec.sort_by_cached_key(|&h| consensus.headers_store.get_timestamp(h).unwrap());
+    vec.sort_by_cached_key(|&h| src_consensus.headers_store.get_timestamp(h).unwrap());
     vec
 }
