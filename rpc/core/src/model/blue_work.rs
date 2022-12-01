42d6b3719977ae549208681c4783e4578c2b9156
Elichai Turkel <elichai.turkel@gmail.com>
2022-12-01 12:50:34 +0200

    Add big endian `from_hex` to Uint* (#84)
    * Add from_hex to Uint
    
    * Use the new Uint192::from_hex method
    
    * Small fmt changes
    
    Co-authored-by: Ori Newman <orinewman1@gmail.com>
    Co-authored-by: Michael Sutton <mikisiton2@gmail.com>

--- rpc/core/src/model/blue_work.rs
+++ rpc/core/src/model/blue_work.rs
@@ -43,19 +43,7 @@ impl FromStr for RpcBlueWorkType {
     type Err = RpcError;
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        // TODO: replace the whole impl with math::Uint192::from_hex when available
-        const HEX_LEN: usize = math::Uint192::BYTES * 2;
-        let mut hex = [0u8; HEX_LEN];
-        if s.len() > HEX_LEN {
-            return Err(RpcError::HexParsingError(faster_hex::Error::InvalidLength(s.len())));
-        }
-        hex.iter_mut().rev().zip(s.bytes().rev()).for_each(|(target, source)| *target = source);
-        if s.len() < HEX_LEN {
-            hex[0..HEX_LEN - s.len()].iter_mut().for_each(|x| *x = b'0');
-        }
-        let mut bytes = [0u8; math::Uint192::BYTES];
-        faster_hex::hex_decode(&hex, &mut bytes)?;
-        Ok(RpcBlueWorkType(BlueWorkType::from_be_bytes(bytes)))
+        BlueWorkType::from_hex(s).map(RpcBlueWorkType).map_err(RpcError::HexParsingError)
     }
 }
 
