697cd8351655fe71404446f7492e069905d0ae30
msutton <mikisiton2@gmail.com>
2023-01-27 13:34:54 +0200

    Fix new clippy uninlined-format-args lint

--- p2p/build.rs
+++ p2p/build.rs
@@ -6,9 +6,9 @@ fn main() {
         .build_server(true)
         .build_client(true)
         .compile(iface_files, dirs)
-        .unwrap_or_else(|e| panic!("protobuf compilation failed, error: {}", e));
+        .unwrap_or_else(|e| panic!("protobuf compilation failed, error: {e}"));
     // recompile protobufs only if any of the proto files changes.
     for file in iface_files {
-        println!("cargo:rerun-if-changed={}", file);
+        println!("cargo:rerun-if-changed={file}");
     }
 }
