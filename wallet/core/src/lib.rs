2960a578a8d69637f3fd8fcce8a1225632ea18ac
Surinder Singh Matoo <surinder83singh@gmail.com>
2023-01-28 06:22:46 +0530

    HD wallet Gen1 tests

--- wallet/core/src/lib.rs
+++ wallet/core/src/lib.rs
@@ -9,3 +9,15 @@ pub use result::Result;
 pub use wallet::Wallet;
 pub use wallets::dummy_address;
 pub use wrapper::WalletWrapper;
+
+#[macro_export]
+macro_rules! hex {
+    ($str: literal) => {{
+        let len = $str.as_bytes().len() / 2;
+        let mut dst = vec![0; len];
+        dst.resize(len, 0);
+        faster_hex::hex_decode_fallback($str.as_bytes(), &mut dst);
+        dst
+    }
+    [..]};
+}
