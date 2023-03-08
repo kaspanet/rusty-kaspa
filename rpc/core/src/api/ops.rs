4c11e6414249e1cb5822ddd6c1a6730d502067d8
CEPEC\georges.kuenzli <georges.kuenzli@cepec.com>
2023-03-08 15:29:11 +0100

    Order ops and disable temporarily the seal macro

--- rpc/core/src/api/ops.rs
+++ rpc/core/src/api/ops.rs
@@ -1,8 +1,8 @@
 use borsh::{BorshDeserialize, BorshSchema, BorshSerialize};
 use serde::{Deserialize, Serialize};
-use workflow_core::{enums::Describe, seal};
+use workflow_core::{enums::Describe};
 
-seal!(0x121c, {
+//seal!(0x0a6c, {
     // ^^^^^ NOTE: This enum is used for binary RPC data exchange, if you
     //             add any new variants to this enum, please inform the
     //             core development team to facilitate a protocol update.
@@ -16,49 +16,48 @@ seal!(0x121c, {
     #[derive(Describe, Clone, Debug, PartialEq, Eq, Hash, BorshSerialize, BorshDeserialize, Serialize, Deserialize)]
     #[serde(rename_all = "camelCase")]
     pub enum RpcApiOps {
+        Ping = 0,
+        GetProcessMetrics,
+        GetCurrentNetwork,
+        SubmitBlock,
+        GetBlockTemplate,
+        GetPeerAddresses,
+        GetSelectedTipHash,
+        GetMempoolEntry,
+        GetMempoolEntries,
+        GetConnectedPeerInfo,
         AddPeer,
-        Ban,
-        EstimateNetworkHashesPerSecond,
-        GetBalanceByAddress,
-        GetBalancesByAddresses,
+        SubmitTransaction,
         GetBlock,
+        GetSubnetwork,
+        GetVirtualSelectedParentChainFromBlock,
+        GetBlocks,
         GetBlockCount,
         GetBlockDagInfo,
-        GetBlocks,
-        GetBlockTemplate,
-        GetCoinSupply,
-        GetConnectedPeerInfo,
-        GetCurrentNetwork,
-        GetHeaders,
-        GetInfo,
-        GetMempoolEntries,
-        GetMempoolEntriesByAddresses,
-        GetMempoolEntry,
-        GetPeerAddresses,
-        GetProcessMetrics,
-        GetSelectedTipHash,
-        GetSubnetwork,
-        GetUtxosByAddresses,
-        GetVirtualSelectedParentBlueScore,
-        GetVirtualSelectedParentChainFromBlock,
-        Ping,
         ResolveFinalityConflict,
         Shutdown,
-        SubmitBlock,
-        SubmitTransaction,
+        GetHeaders,
+        GetUtxosByAddresses,
+        GetBalanceByAddress,
+        GetBalancesByAddresses,
+        GetVirtualSelectedParentBlueScore,
+        Ban,
         Unban,
+        GetInfo,
+        EstimateNetworkHashesPerSecond,
+        GetMempoolEntriesByAddresses,
+        GetCoinSupply,
 
         // Subscription commands for starting/stopping notifications
         NotifyBlockAdded,
-        NotifyFinalityConflict,
-        NotifyFinalityConflictResolved, // TODO added to match NotificationType
-        NotifyFinalityConflicts,        // TODO - missing in NotificationType
         NotifyNewBlockTemplate,
-        NotifyPruningPointUtxoSetOverride,
         NotifyUtxosChanged,
+        NotifyPruningPointUtxoSetOverride,
+        NotifyFinalityConflict,
+        NotifyFinalityConflictResolved, // for uniformity purpose only since subscribing to NotifyFinalityConflict means receiving both FinalityConflict and FinalityConflictResolved
         NotifyVirtualDaaScoreChanged,
-        NotifyVirtualSelectedParentBlueScoreChanged,
         NotifyVirtualSelectedParentChainChanged,
+        NotifyVirtualSelectedParentBlueScoreChanged,
 
         // ~
         Subscribe,
@@ -67,7 +66,7 @@ seal!(0x121c, {
         // Server to client notification
         Notification,
     }
-});
+//});
 
 impl From<RpcApiOps> for u32 {
     fn from(item: RpcApiOps) -> Self {
@@ -97,14 +96,3 @@ impl From<i32> for SubscribeCommand {
         }
     }
 }
-
-// TODO - remove or change?
-// #[cfg(test)]
-// mod tests {
-//     use super::RpcApiOps;
-
-//     #[test]
-//     fn test_rpc_api_ops_convert() {
-//         assert_eq!(0_u32, u32::from(RpcApiOps::Ping));
-//     }
-// }
