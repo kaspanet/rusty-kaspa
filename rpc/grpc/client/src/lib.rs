cfd965aaf47d548b135f97d34e32259c03e0c7b6
CEPEC\georges.kuenzli <georges.kuenzli@cepec.com>
2023-03-11 18:48:46 +0100

    Make gRPC client notifier public

--- rpc/grpc/client/src/lib.rs
+++ rpc/grpc/client/src/lib.rs
@@ -68,7 +68,7 @@ impl GrpcClient {
     }
 
     #[inline(always)]
-    fn notifier(&self) -> Arc<Notifier<Notification, ChannelConnection>> {
+    pub fn notifier(&self) -> Arc<Notifier<Notification, ChannelConnection>> {
         self.notifier.clone()
     }
 
