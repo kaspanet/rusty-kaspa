44851be3ea0954622d8c82dbd988cffe009bb23c
Tiram <18632023+tiram88@users.noreply.github.com>
2023-03-18 15:47:44 +0000

    Decouple ci and check script

--- check.ps1
+++ check.ps1
@@ -2,17 +2,17 @@ cargo fmt --all
 cargo clippy --workspace --tests --benches -- -D warnings
 
 $crates = @(
-  "kaspa-core",
-  "consensus-core",
-  "addresses",
-  "hashes",
-  "math",
-  "kaspa-rpc-core",
-  "kaspa-bip32",
-  "kaspa-wrpc-client",
+  # "kaspa-core",
+  # "consensus-core",
+  # "addresses",
+  # "hashes",
+  # "math",
+  # "kaspa-rpc-core",
+  # "kaspa-bip32",
+  # "kaspa-wrpc-client",
   "kaspa-wrpc-wasm",
-  "kaspa-wallet-core",
-  "kaspa-wallet-cli",
+  # "kaspa-wallet-core",
+  # "kaspa-wallet-cli",
   "kaspa-wallet-cli-wasm",
   "kaspa-wasm"
 )
@@ -20,6 +20,12 @@ $crates = @(
 $env:AR="llvm-ar"
 foreach ($crate in $crates)
 {
+  Write-Output "`ncargo clippy -p $crate --target wasm32-unknown-unknown"
   cargo clippy -p $crate --target wasm32-unknown-unknown
+  $status=$LASTEXITCODE
+  if($status -ne 0) {
+    Write-Output "`n--> wasm32 check of $crate failed`n"
+    break
+  }
 }
 $env:AR=""
