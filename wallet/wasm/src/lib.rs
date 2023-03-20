069ab5caa05f39cd587a8d2d634987b548eeef76
Anton Yemelyanov <anton.yemelyanov@gmail.com>
2023-03-20 01:59:06 -0400

    remove automatic wallet startup in wasm

--- wallet/wasm/src/lib.rs
+++ wallet/wasm/src/lib.rs
@@ -3,7 +3,7 @@ use wasm_bindgen::prelude::*;
 use workflow_terminal::Options;
 use workflow_terminal::Result;
 
-#[wasm_bindgen(start)]
+#[wasm_bindgen]
 pub async fn load_kaspa_wallet_cli() -> Result<()> {
     workflow_wasm::panic::init_console_panic_hook();
     let options = Options { ..Options::default() };
