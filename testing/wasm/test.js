cd3a80f9f05c40dce5386608bfbfd808ebf240a9
KaffinPX <kaffinpx@gmail.com>
2023-04-10 18:21:52 +0300

    Add address tests

--- testing/wasm/test.js
+++ testing/wasm/test.js
@@ -1,5 +1,24 @@
-describe('proving tests works', () => {
-  it ('proves tests works', async () => {
-    
+const kaspa = require('./kaspa')
+const assert = require('assert')
+
+const testData = require('./src/testData.json')
+
+describe ('handling wallets', () => {
+  it ('handles addresses', () => {
+    const validAddress = new kaspa.Address(testData.wallet.address.valid)
+    //TODO: Do assert test for validAddress when hex fixed
+
+    const invalidAddresses = Object.values(testData.wallet.address.invalid)
+    let detectedInvalids = 0
+
+    invalidAddresses.forEach(address => {
+      try {
+        new kaspa.Address(address)
+      } catch {
+        detectedInvalids += 1
+      }
+    })
+
+    assert.strictEqual(invalidAddresses.length, detectedInvalids, `Theres ${invalidAddresses.length} invalid addresses but only detected ${detectedInvalids}`)
   })
 })
