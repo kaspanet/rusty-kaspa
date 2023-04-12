60ccb2607d231d7feb6900bb91aa70de574aca85
Anton Yemelyanov <anton.yemelyanov@gmail.com>
2023-04-12 12:33:03 -0400

    fix module reference (websocket->isomorphic-ws)

--- wasm/npm/index.js
+++ wasm/npm/index.js
@@ -1,3 +1,3 @@
-globalThis.WebSocket = require('websocket').w3cwebsocket;
-
+globalThis.WebSocket = require('isomorphic-ws');
+// globalThis.WebSocket = require('websocket').w3cwebsocket;
 module.exports = require('./kaspa/kaspa_wasm');
