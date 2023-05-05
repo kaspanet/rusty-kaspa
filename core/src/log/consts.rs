1c44ef43e1a7883e18631e1d9debfc4dcbc1bf56
Tiram <18632023+tiram88@users.noreply.github.com>
2023-05-05 21:23:05 +0000

    Apply original 100 MB max log file size

--- core/src/log/consts.rs
+++ core/src/log/consts.rs
@@ -7,7 +7,7 @@ pub const LOG_ARCHIVE_SUFFIX: &str = ".{}.gz";
 
 pub const LOG_FILE_BASE_ROLLS: u32 = 1;
 pub const LOG_FILE_MAX_ROLLS: u32 = 8;
-pub const LOG_FILE_MAX_SIZE: u64 = 5 * 1_000_000;
+pub const LOG_FILE_MAX_SIZE: u64 = 100_000_000;
 
 pub const LOG_LINE_PATTERN_COLORED: &str = "{d(%Y-%m-%dT%H:%M:%S %Z):19.19}Z [{h({({l}):5.5})}] {m}{n}";
 pub const LOG_LINE_PATTERN: &str = "{d(%Y-%m-%dT%H:%M:%S %Z):19.19}Z [{({l}):5.5}] {m}{n}";
