53b8ab663dbd748cc678b04d39d82a264d458488
Tiram <18632023+tiram88@users.noreply.github.com>
2023-05-07 12:08:57 +0000

    Add milliseconds to the log line pattern

--- core/src/log/consts.rs
+++ core/src/log/consts.rs
@@ -9,5 +9,7 @@ pub const LOG_FILE_BASE_ROLLS: u32 = 1;
 pub const LOG_FILE_MAX_ROLLS: u32 = 8;
 pub const LOG_FILE_MAX_SIZE: u64 = 100_000_000;
 
-pub const LOG_LINE_PATTERN_COLORED: &str = "{d(%Y-%m-%dT%H:%M:%S %Z):19.19}Z [{h({({l}):5.5})}] {m}{n}";
-pub const LOG_LINE_PATTERN: &str = "{d(%Y-%m-%dT%H:%M:%S %Z):19.19}Z [{({l}):5.5}] {m}{n}";
+/// Console (stdout) log line pattern, with explicit UTC time zone denoted by the suffix Z
+pub const LOG_LINE_PATTERN_COLORED: &str = "{d(%Y-%m-%d %H:%M:%S%.3f)}Z [{h({({l}):5.5})}] {m}{n}";
+/// File log line pattern, with explicit UTC time zone denoted by the suffix Z
+pub const LOG_LINE_PATTERN: &str = "{d(%Y-%m-%d %H:%M:%S%.3f)}Z [{({l}):5.5}] {m}{n}";
