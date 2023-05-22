f174e14dfbd7fd6b01da85a35e4aea6cc0f4fedc
tmrlvi <tmrlvi@gmail.com>
2023-05-22 10:44:24 +0300

    Ser-de opcode fixes (better api) and some opcode tests (#183)
    * made deserialization easier, serialization now serialize the whole opcode, and added basic tests
    
    * renamed deserialize_opcode_data to deserialize_next_opcode
    
    ---------
    
    Co-authored-by: Ori Newman <orinewman1@gmail.com>

--- crypto/txscript/src/opcodes/macros.rs
+++ crypto/txscript/src/opcodes/macros.rs
@@ -3,7 +3,7 @@ macro_rules! opcode_serde {
         #[allow(dead_code)]
         fn serialize(&self) -> Vec<u8> {
             let length = self.data.len() as $type;
-            [length.to_le_bytes().as_slice(), self.data.as_slice()].concat()
+            [[self.value()].as_slice(), length.to_le_bytes().as_slice(), self.data.as_slice()].concat()
         }
 
         fn deserialize<'i, I: Iterator<Item = &'i u8>, T: VerifiableTransaction>(
@@ -16,6 +16,8 @@ macro_rules! opcode_serde {
                     if data.len() != length {
                         return Err(TxScriptError::MalformedPush(length, data.len()));
                     }
+                    // Skipping the extra check - we already checked data length
+                    // fits
                     Ok(Box::new(Self { data }))
                 }
                 Err(vec) => {
@@ -27,7 +29,7 @@ macro_rules! opcode_serde {
     ($length: literal) => {
         #[allow(dead_code)]
         fn serialize(&self) -> Vec<u8> {
-            self.data.clone()
+            [[self.value()].as_slice(), self.data.clone().as_slice()].concat()
         }
 
         fn deserialize<'i, I: Iterator<Item = &'i u8>, T: VerifiableTransaction>(
@@ -35,6 +37,22 @@ macro_rules! opcode_serde {
         ) -> Result<Box<dyn OpCodeImplementation<T>>, TxScriptError> {
             // Static length includes the opcode itself
             let data: Vec<u8> = it.take($length - 1).copied().collect();
+            Self::new(data)
+        }
+    };
+}
+
+macro_rules! opcode_init {
+    ($type:ty) => {
+        fn new(data: Vec<u8>) -> Result<Box<dyn OpCodeImplementation<T>>, TxScriptError> {
+            if data.len() > <$type>::MAX as usize {
+                return Err(TxScriptError::MalformedPush(<$type>::MAX as usize, data.len()));
+            }
+            Ok(Box::new(Self { data }))
+        }
+    };
+    ($length: literal) => {
+        fn new(data: Vec<u8>) -> Result<Box<dyn OpCodeImplementation<T>>, TxScriptError> {
             if data.len() != $length - 1 {
                 return Err(TxScriptError::MalformedPush($length - 1, data.len()));
             }
@@ -47,18 +65,16 @@ macro_rules! opcode_impl {
     ($name: ident, $num: literal, $length: tt, $code: expr, $self:ident, $vm:ident ) => {
         type $name = OpCode<$num>;
 
-        impl $name {
+        impl OpcodeSerialization for $name {
             opcode_serde!($length);
         }
 
         impl<T: VerifiableTransaction> OpCodeExecution<T> for $name {
-            fn empty() -> Box<dyn OpCodeImplementation<T>> {
-                return Box::new(Self{data: vec![]})
+            fn empty() -> Result<Box<dyn OpCodeImplementation<T>>, TxScriptError> {
+                Self::new(vec![])
             }
 
-            fn new(data: Vec<u8>) -> Box<dyn OpCodeImplementation<T>> {
-                return Box::new(Self{data})
-            }
+            opcode_init!($length);
 
             #[allow(unused_variables)]
             fn execute(&$self, $vm: &mut TxScriptEngine<T>) -> OpCodeResult {
@@ -88,13 +104,21 @@ macro_rules! opcode_list {
 
         $(
             opcode_impl!($name, $num, $length, $code, $self, $vm);
+
+            $(
+                #[allow(dead_code)]
+                type $alias = $name;
+            )?
         )*
 
-        pub fn deserialize_opcode_data<'i, I: Iterator<Item = &'i u8>, T: VerifiableTransaction>(opcode_num: u8, it: &mut I) -> Result<Box<dyn OpCodeImplementation<T>>, TxScriptError> {
-            match opcode_num {
-                $(
-                    $num => $name::deserialize(it),
-                )*
+        pub fn deserialize_next_opcode<'i, I: Iterator<Item = &'i u8>, T: VerifiableTransaction>(it: &mut I) -> Option<Result<Box<dyn OpCodeImplementation<T>>, TxScriptError>> {
+            match it.next() {
+                Some(opcode_num) => match opcode_num {
+                    $(
+                        $num => Some($name::deserialize(it)),
+                    )*
+                },
+                _ => None
             }
         }
     };
