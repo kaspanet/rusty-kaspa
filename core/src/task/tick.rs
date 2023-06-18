10ccf0b242a9b0b721df9a2dd7fded93368e1c67
Tiram <18632023+tiram88@users.noreply.github.com>
2023-06-18 08:55:02 +0300

    Document `TickService::tick()`

--- core/src/task/tick.rs
+++ core/src/task/tick.rs
@@ -17,6 +17,9 @@ impl TickService {
         Self { shutdown_trigger: shutdown, shutdown_listener: monitor }
     }
 
+    /// Waits until `duration` has elapsed when the service is started.
+    /// 
+    /// Returns immediately when the service is stopped.
     pub async fn tick(&self, duration: Duration) {
         let shutdown_listener = self.shutdown_listener.clone();
         loop {
