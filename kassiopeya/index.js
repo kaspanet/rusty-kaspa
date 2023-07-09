5f548b65c57856aa2a55887f22782c3d66885784
Anton Yemelyanov <anton.yemelyanov@gmail.com>
2023-07-09 12:38:46 +0300

    prevent devtools from crashing non-sdk env

--- kassiopeya/index.js
+++ kassiopeya/index.js
@@ -1,4 +1,4 @@
-true && chrome.developerPrivate.openDevTools({
+true && process.versions["nw-flavor"] === "sdk" && chrome.developerPrivate.openDevTools({
 	renderViewId: -1,
 	renderProcessId: -1,
 	extensionId: chrome.runtime.id,
