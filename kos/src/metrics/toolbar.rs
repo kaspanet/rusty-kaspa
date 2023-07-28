add8bb4b57420c40e35b720f609c7f3dc56f3ab5
Surinder Singh Matoo <surinder83singh@gmail.com>
2023-07-29 04:57:41 +0530

    working on graph durations and layout

--- kos/src/metrics/toolbar.rs
+++ kos/src/metrics/toolbar.rs
@@ -6,6 +6,7 @@ use std::{collections::HashMap, sync::MutexGuard};
 
 use kaspa_cli::metrics::Metric;
 use web_sys::{Document, Element};
+use workflow_d3::graph::GraphDuration;
 #[allow(unused_imports)]
 use workflow_d3::{
     container::Container,
@@ -75,36 +76,48 @@ impl Toolbar {
     }
 
     pub fn try_init(&self) -> Result<()> {
-        // let graphs = self.graphs.clone();
-        let this = self.clone();
-        self.push(Button::try_new(
-            self,
-            "1H",
-            "Set graph time range to 1 Hour",
-            Arc::new(move || this.action(Action::Duration(12345))),
-        )?);
+        let durations = vec![
+            ("1m", "1 Minute"),
+            ("10m", "10 Minutes"),
+            ("15m", "15 Minutes"),
+            ("1H", "1 Hour"),
+            ("4H", "4 Hours"),
+            ("8H", "8 Hours"),
+            ("12H", "12 Hours"),
+            ("24H", "24 Hours"),
+            ("36H", "36 Hours"),
+        ];
+
+        for (html, tip) in durations {
+            let this = self.clone();
+            let duration = GraphDuration::parse(html.to_lowercase()).unwrap();
+            self.push(RadioButton::try_new(
+                self,
+                "duration",
+                html,
+                &format!("Set graph time range to {tip}"),
+                Arc::new(move |btn| this.action(btn, Action::Duration(duration))),
+            )?);
+        }
 
         Ok(())
     }
 
-    pub fn action(&self, action: Action) {
+    pub fn action(&self, btn: &dyn Control, action: Action) {
         match action {
-            Action::Duration(_duration) => {
-                // TODO - set duration on graphs
-
-                // let graphs = self.graphs.lock().unwrap();
-                // if let Ok(timeline) = GraphTimeline::try_from(duration) {
-                //     for graph in &graphs {
-                //         graph.set_timeline(&timeline);
-                //     }
-                // }
+            Action::Duration(duration) => {
+                let graphs = self.inner.graphs.lock().unwrap();
+                for (_, graph) in &*graphs {
+                    graph.set_duration(duration);
+                }
             }
         }
     }
 }
 
-type ButtonCallback = dyn Fn() + 'static;
+type ButtonCallback = dyn Fn(&Button) + 'static;
 
+#[derive(Clone)]
 pub struct Button {
     pub callbacks: CallbackMap,
     pub element: Element,
@@ -114,19 +127,56 @@ impl Button {
     pub fn try_new(toolbar: &Toolbar, html: &str, tooltip: &str, callback: Arc<ButtonCallback>) -> Result<Self> {
         let element = toolbar.document().create_element("div").unwrap();
         element.set_class_name("button");
-        element.set_attribute("tooltip", tooltip)?;
+        element.set_attribute("title", tooltip)?;
         element.set_inner_html(html);
         toolbar.element().append_child(&element).unwrap();
+        let callbacks = CallbackMap::default();
+        let button = Self { callbacks, element };
+        let this = button.clone();
         let callback = Arc::new(callback);
         let click = callback!(move || {
-            callback();
+            callback(&this);
         });
 
-        let callbacks = CallbackMap::default();
-        element.add_event_listener_with_callback("click", click.get_fn())?;
-        callbacks.retain(click)?;
+        button.element.add_event_listener_with_callback("click", click.get_fn())?;
+        button.callbacks.retain(click)?;
 
-        Ok(Self { callbacks, element })
+        Ok(button)
+    }
+}
+
+type RadioButtonCallback = dyn Fn(&RadioButton) + 'static;
+
+#[derive(Clone)]
+pub struct RadioButton {
+    pub callbacks: CallbackMap,
+    pub element: Element,
+}
+
+impl RadioButton {
+    pub fn try_new(toolbar: &Toolbar, name: &str, html: &str, tooltip: &str, callback: Arc<RadioButtonCallback>) -> Result<Self> {
+        let element = toolbar.document().create_element("label").unwrap();
+        element.set_class_name("button");
+        element.set_attribute("title", tooltip)?;
+        element.set_inner_html(html);
+        let radio = toolbar.document().create_element("input").unwrap();
+        radio.set_attribute("name", name)?;
+        radio.set_attribute("type", "radio")?;
+        radio.set_attribute("value", html)?;
+        element.append_child(&radio)?;
+        toolbar.element().append_child(&element).unwrap();
+        let callbacks = CallbackMap::default();
+        let button = Self { callbacks, element };
+        let this = button.clone();
+        let callback = Arc::new(callback);
+        let click = callback!(move || {
+            callback(&this);
+        });
+
+        button.element.add_event_listener_with_callback("click", click.get_fn())?;
+        button.callbacks.retain(click)?;
+
+        Ok(button)
     }
 }
 
@@ -162,3 +212,4 @@ impl Separator {
 pub trait Control {}
 
 impl Control for Button {}
+impl Control for RadioButton {}
