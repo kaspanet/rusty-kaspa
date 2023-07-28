ab9026a01732b0db543a056798c8e3fffa7ac111
Anton Yemelyanov <anton.yemelyanov@gmail.com>
2023-07-28 20:34:05 +0300

    add metrics value formatting strings

--- cli/src/metrics/data.rs
+++ cli/src/metrics/data.rs
@@ -1,4 +1,5 @@
 use borsh::{BorshDeserialize, BorshSerialize};
+use separator::Separatable;
 use serde::{Deserialize, Serialize};
 use wasm_bindgen::prelude::*;
 use workflow_core::{enums::Describe, sendable::Sendable};
@@ -48,6 +49,8 @@ impl MetricsData {
 
 #[derive(Default, Debug, Clone, BorshDeserialize, BorshSerialize, Serialize, Deserialize)]
 pub struct MetricsSnapshot {
+    pub data: MetricsData,
+
     pub unixtime: f64,
     pub duration: f64,
     // ---
@@ -70,22 +73,42 @@ pub struct MetricsSnapshot {
 impl MetricsSnapshot {
     pub fn get(&self, metric: &Metric) -> Sendable<JsValue> {
         let v = match metric {
-            Metric::BlocksSubmitted => JsValue::from(self.blocks_submitted as f64),
-            Metric::HeaderCount => JsValue::from(self.header_counts as f64),
-            Metric::DepCounts => JsValue::from(self.dep_counts as f64),
-            Metric::BodyCounts => JsValue::from(self.body_counts as f64),
-            Metric::TxnCounts => JsValue::from(self.txs_counts as f64),
-            Metric::ChainBlockCounts => JsValue::from(self.chain_block_counts as f64),
-            Metric::MassCounts => JsValue::from(self.mass_counts as f64),
-            Metric::BlockCount => JsValue::from(self.block_count as f64),
-            Metric::TipHashes => JsValue::from(self.tip_hashes as f64),
+            Metric::BlocksSubmitted => JsValue::from(self.blocks_submitted),
+            Metric::HeaderCount => JsValue::from(self.header_counts),
+            Metric::DepCounts => JsValue::from(self.dep_counts),
+            Metric::BodyCounts => JsValue::from(self.body_counts),
+            Metric::TxnCounts => JsValue::from(self.txs_counts),
+            Metric::ChainBlockCounts => JsValue::from(self.chain_block_counts),
+            Metric::MassCounts => JsValue::from(self.mass_counts),
+            Metric::BlockCount => JsValue::from(self.block_count),
+            Metric::TipHashes => JsValue::from(self.tip_hashes),
             Metric::Difficulty => JsValue::from(self.difficulty),
-            Metric::PastMedianTime => JsValue::from(self.past_median_time as f64),
-            Metric::VirtualParentHashes => JsValue::from(self.virtual_parent_hashes as f64),
-            Metric::VirtualDaaScore => JsValue::from(self.virtual_daa_score as f64),
+            Metric::PastMedianTime => JsValue::from(self.past_median_time),
+            Metric::VirtualParentHashes => JsValue::from(self.virtual_parent_hashes),
+            Metric::VirtualDaaScore => JsValue::from(self.virtual_daa_score),
         };
 
         Sendable(v)
+    }
+
+    pub fn format(&self, metric: &Metric) -> String {
+        match metric {
+            Metric::BlocksSubmitted => format!("Blocks Submitted: {}", self.blocks_submitted.separated_string()),
+            Metric::HeaderCount => format!("Headers: {}", self.header_counts.separated_string()),
+            Metric::DepCounts => format!("Dependencies: {}", self.dep_counts.separated_string()),
+            Metric::BodyCounts => format!("Body Counts: {}", self.body_counts.separated_string()),
+            Metric::TxnCounts => format!("Transactions: {}", self.txs_counts.separated_string()),
+            Metric::ChainBlockCounts => format!("Chain Blocks: {}", self.chain_block_counts.separated_string()),
+            Metric::MassCounts => format!("Mass Counts: {}", self.mass_counts.separated_string()),
+            Metric::BlockCount => format!("Blocks: {}", self.block_count.separated_string()),
+            Metric::TipHashes => format!("Tip Hashes: {}", self.tip_hashes.separated_string()),
+            Metric::Difficulty => {
+                format!("Difficulty: {}", self.difficulty.separated_string())
+            }
+            Metric::PastMedianTime => format!("Past Median Time: {}", self.past_median_time.separated_string()),
+            Metric::VirtualParentHashes => format!("Virtual Parent Hashes: {}", self.virtual_parent_hashes.separated_string()),
+            Metric::VirtualDaaScore => format!("Virtual DAA Score: {}", self.virtual_daa_score.separated_string()),
+        }
     }
 }
 
@@ -95,20 +118,46 @@ impl From<(&MetricsData, &MetricsData)> for MetricsSnapshot {
             unixtime: b.unixtime,
             duration: b.unixtime - a.unixtime,
             // ---
-            blocks_submitted: b.blocks_submitted as f64,     // - a.blocks_submitted,
-            header_counts: b.header_counts as f64,           // - a.header_counts,
-            dep_counts: b.dep_counts as f64,                 // - a.dep_counts,
-            body_counts: b.body_counts as f64,               // - a.body_counts,
-            txs_counts: b.txs_counts as f64,                 // - a.txs_counts,
-            chain_block_counts: b.chain_block_counts as f64, // - a.chain_block_counts,
-            mass_counts: b.mass_counts as f64,               // - a.mass_counts,
+            blocks_submitted: b.blocks_submitted as f64,
+            header_counts: b.header_counts as f64,
+            dep_counts: b.dep_counts as f64,
+            body_counts: b.body_counts as f64,
+            txs_counts: b.txs_counts as f64,
+            chain_block_counts: b.chain_block_counts as f64,
+            mass_counts: b.mass_counts as f64,
             // ---
-            block_count: b.block_count as f64,                     // - a.block_count,
-            tip_hashes: b.tip_hashes as f64,                       // - a.tip_hashes,
-            difficulty: b.difficulty as f64,                       // - a.difficulty,
-            past_median_time: b.past_median_time as f64,           // - a.past_median_time,
-            virtual_parent_hashes: b.virtual_parent_hashes as f64, // - a.virtual_parent_hashes,
-            virtual_daa_score: b.virtual_daa_score as f64,         // - a.virtual_daa_score,
+            block_count: b.block_count as f64,
+            tip_hashes: b.tip_hashes as f64,
+            difficulty: b.difficulty,
+            past_median_time: b.past_median_time as f64,
+            virtual_parent_hashes: b.virtual_parent_hashes as f64,
+            virtual_daa_score: b.virtual_daa_score as f64,
+
+            data: b.clone(),
         }
     }
 }
+
+// impl From<(&MetricsData, &MetricsData)> for MetricsSnapshot {
+//     fn from((a, b): (&MetricsData, &MetricsData)) -> Self {
+//         Self {
+//             unixtime: b.unixtime,
+//             duration: b.unixtime - a.unixtime,
+//             // ---
+//             blocks_submitted: b.blocks_submitted as f64,     // - a.blocks_submitted,
+//             header_counts: b.header_counts as f64,           // - a.header_counts,
+//             dep_counts: b.dep_counts as f64,                 // - a.dep_counts,
+//             body_counts: b.body_counts as f64,               // - a.body_counts,
+//             txs_counts: b.txs_counts as f64,                 // - a.txs_counts,
+//             chain_block_counts: b.chain_block_counts as f64, // - a.chain_block_counts,
+//             mass_counts: b.mass_counts as f64,               // - a.mass_counts,
+//             // ---
+//             block_count: b.block_count as f64,                     // - a.block_count,
+//             tip_hashes: b.tip_hashes as f64,                       // - a.tip_hashes,
+//             difficulty: b.difficulty as f64,                       // - a.difficulty,
+//             past_median_time: b.past_median_time as f64,           // - a.past_median_time,
+//             virtual_parent_hashes: b.virtual_parent_hashes as f64, // - a.virtual_parent_hashes,
+//             virtual_daa_score: b.virtual_daa_score as f64,         // - a.virtual_daa_score,
+//         }
+//     }
+// }
