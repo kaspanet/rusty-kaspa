b795b51e01cd451a8af8efb2b1375ed6727af5e1
Anton Yemelyanov <anton.yemelyanov@gmail.com>
2023-08-01 18:46:14 +0300

    wip - help now uses term().help()

--- cli/src/modules/help.rs
+++ cli/src/modules/help.rs
@@ -7,19 +7,14 @@ pub struct Help;
 impl Help {
     async fn main(self: Arc<Self>, dyn_ctx: &Arc<dyn Context>, _argv: Vec<String>, _cmd: &str) -> Result<()> {
         let term = dyn_ctx.term();
-        term.writeln("\nCommands:\n".crlf());
+        term.writeln("\nCommands:".crlf());
 
         let ctx = dyn_ctx.clone().downcast_arc::<KaspaCli>()?;
         let handlers = ctx.handlers().collect();
-        let mut handlers =
+        let handlers =
             handlers.into_iter().filter_map(|h| h.verb(dyn_ctx).map(|verb| (verb, get_handler_help(h, dyn_ctx)))).collect::<Vec<_>>();
 
-        handlers.sort_by_key(|(verb, _)| verb.to_string());
-        let len = handlers.iter().map(|(c, _)| c.len()).fold(0, |a, b| a.max(b)) + 2;
-        for (verb, help) in handlers {
-            term.writeln(format!("{:>4} {} {}", "", verb.pad_to_width(len), help));
-        }
-        term.writeln("");
+        term.help(&handlers, None)?;
 
         Ok(())
     }
