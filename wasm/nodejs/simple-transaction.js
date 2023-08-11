382fae9cd5725cf21b3b514a3e5dba4552a0958f
Dennis Kievits <denkievits@gmail.com>
2023-08-11 21:36:01 +0200

    Modified utils.js to allow for more customisation, fixed issues. Updated simple-transaction.js to use `parseArgs`.

--- wasm/nodejs/simple-transaction.js
+++ wasm/nodejs/simple-transaction.js
@@ -12,51 +12,61 @@ const {
     createTransactions,
     init_console_panic_hook
 } = require('./kaspa/kaspa_wasm');
+const {parseArgs} = require('./utils');
 
 init_console_panic_hook();
 
 async function runDemo() {
+    const {
+        address,
+        networkType,
+        encoding,
+    } = parseArgs({
+        additionalHelpOutput: "[--destination <address>]",
+    });
+    if (address === null) {
+        console.error('No address specified. Use --help for more information.');
+        return;
+    }
     let args = process.argv.slice(2);
-    let destination = args.shift() || "kaspatest:qqkl0ct62rv6dz74pff2kx5sfyasl4z28uekevau23g877r5gt6userwyrmtt";
+    let destination = "kaspatest:qqkl0ct62rv6dz74pff2kx5sfyasl4z28uekevau23g877r5gt6userwyrmtt";
+
+    const destinationArgIdx = args.findIndex((arg) => arg === '--destination');
+    if (destinationArgIdx !== -1) {
+        destination = args[destinationArgIdx + 1];
+    }
     console.log("using destination address:", destination);
-    
-    // ---
-    // network type
-    let network = NetworkType.Testnet;
-    // RPC encoding
-    let encoding = Encoding.Borsh;
-    // ---
 
     // From BIP0340
     const sk = new PrivateKey('b7e151628aed2a6abf7158809cf4f3c762e7160f38b4da56a784d9045190cfef');
 
-    const kaspaAddress = sk.toKeypair().toAddress(network).toString();
+    const kaspaAddress = sk.toKeypair().toAddress(networkType).toString();
     // Full kaspa address: kaspa:qr0lr4ml9fn3chekrqmjdkergxl93l4wrk3dankcgvjq776s9wn9jkdskewva
     console.info(`Full kaspa address: ${kaspaAddress}`);
 
-    const address = new Address(kaspaAddress);
+    // const address = new Address(kaspaAddress);
     console.info(address);
     console.info(sk.toKeypair().xOnlyPublicKey); // dff1d77f2a671c5f36183726db2341be58feae1da2deced843240f7b502ba659
     console.info(sk.toKeypair().publicKey);      // 02dff1d77f2a671c5f36183726db2341be58feae1da2deced843240f7b502ba659
 
-    let rpcUrl = RpcClient.parseUrl("127.0.0.1", encoding, network);
-    const rpc = new RpcClient(encoding, rpcUrl, network);
+    let rpcUrl = RpcClient.parseUrl("127.0.0.1", encoding, networkType);
+    const rpc = new RpcClient(encoding, rpcUrl, networkType);
     console.log(`Connecting to ${rpc.url}`);
 
     await rpc.connect();
 
     let entries = await rpc.getUtxosByAddresses([address]);
-    
+
     if (!entries.length) {
         console.error("No UTXOs found for address");
     } else {
         console.info(entries);
-        
+
         // a very basic JS-driven utxo entry sort
         entries.sort((a, b) => a.utxoEntry.amount > b.utxoEntry.amount || -(a.utxoEntry.amount < b.utxoEntry.amount));
 
         let { transactions, summary } = await createTransactions({
-            entries, 
+            entries,
             outputs : [[destination, kaspaToSompi(0.2)]],
             priorityFee: 0,
             changeAddress: address,
--- wasm/nodejs/utils.js
+++ wasm/nodejs/utils.js
@@ -6,31 +6,39 @@ const {
     NetworkType,
 } = require('./kaspa/kaspa_wasm');
 
-function parseArgs() {
-
+function parseArgs(options = {}) {
     const script = path.basename(process.argv[1]);
 
-    let args = process.argv.slice(2).join(" ");
-
-    if (args.match(/--help/i)) {
-        console.log(`Usage: node ${script} [address] [mainnet|testnet] [--json]`);
+    let args = process.argv.slice(2);
+    if (args.includes('--help')) {
+        console.log(`Usage: node ${script} [address] [mainnet|testnet] [--json] ${options.additionalHelpOutput}`);
+        process.exit(0);
     }
+
+    let addressRegex = new RegExp(/(kaspa|kaspatest):\S+/i);
+    const addressArg = args.find((arg, i) => i === 0 && addressRegex.test(arg));
+    const networkArg = args.find((arg, i) => (i === 0 || i === 1) && (arg === 'mainnet' || arg === 'testnet'));
 
     let address = null;
-    let addressRegex = new RegExp(/(kaspa|kaspatest):\S+/i);
-    if (args.match(addressRegex)) {
-        address = new Address(args.match(addressRegex)[0]);
+    if (addressArg !== undefined) {
+        if (addressRegex.test(addressArg)) {
+            address = new Address(addressArg);
+        }
     }
 
-    let networkType = NetworkType = NetworkType.Mainnet;
-    if (args.match(/mainnet/i)) {
-        networkType = NetworkType.Mainnet;
-    } else if (address && address.startsWith("kaspa:")) {
-        networkType = NetworkType.Mainnet;
+    // by default, use testnet
+    let networkType = NetworkType.Testnet;
+    // if "mainnet" is specified or if address starts with "kaspa:" use mainnet
+    if (networkArg !== undefined) {
+        if (networkArg === 'mainnet' || addressArg.startsWith('kaspa:')) {
+            networkType = NetworkType.Mainnet;
+        }
     }
 
     let encoding = Encoding.Borsh;
-    if (args.match(/--json/i)) {
+
+    const jsonArgIdx = args.findIndex((arg) => arg === '--json');
+    if (jsonArgIdx !== -1) {
         encoding = Encoding.SerdeJson;
     }
 
