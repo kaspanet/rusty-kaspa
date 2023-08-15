ef0a7e7703e5604dcf18aea363d61b9842cfa8bb
Dennis Kievits <denkievits@gmail.com>
2023-08-15 23:11:42 +0200

    progress working on parse host function

---
+++ rpc/wrpc/client/src/parse.rs
@@ -0,0 +1,184 @@
+use std::net::{Ipv4Addr, Ipv6Addr};
+use std::num::ParseIntError;
+
+#[derive(Debug, PartialEq, Eq)]
+pub struct ParseHostOutput<'a> {
+    scheme: Option<&'a str>,
+    host: Host<'a>,
+    port: Option<u16>,
+}
+
+#[derive(Debug, PartialEq, Eq)]
+pub enum Host<'a> {
+    Domain(&'a str),
+    Hostname(&'a str),
+    Ipv4(Ipv4Addr),
+    Ipv6(Ipv6Addr),
+}
+
+#[derive(Debug, thiserror::Error, PartialEq, Eq)]
+pub enum ParseHostError {
+    #[error("Invalid input")]
+    InvalidInput,
+    #[error("Invalid port: {0}")]
+    ParsePortError(ParseIntError),
+}
+
+pub fn parse_host(input: &str) -> Result<ParseHostOutput, ParseHostError> {
+    // Attempt to split the input into scheme, host, and port.
+    let (scheme, input) = match input.find("://") {
+        Some(pos) => {
+            let (scheme, input) = input.split_at(pos);
+            (Some(scheme), &input[3..])
+        }
+        None => (None, input),
+    };
+    // Attempt to split path and host.
+    let (input, _path) = match input.find('/') {
+        Some(pos) => input.split_at(pos),
+        None => (input, ""),
+    };
+
+    let (host, port_str) = match input.rfind(':') {
+        Some(pos) => {
+            // Check if char before ':' is also ':'.
+            // As that would mean that the ':' is part of an IPv6 address.
+            if input.chars().nth(pos - 1) == Some(':') {
+                (input, None)
+            } else {
+                let (host, port_str) = input.split_at(pos);
+                (host, Some(&port_str[1..]))
+            }
+        }
+        None => (input, None),
+    };
+
+    if host.is_empty() {
+        return Err(ParseHostError::InvalidInput);
+    }
+
+    let port = port_str
+        .map(|port_str| match port_str.parse::<u16>() {
+            Ok(port) => Ok(port),
+            Err(err) => Err(ParseHostError::ParsePortError(err)),
+        })
+        .map_or(Ok(None), |port| port.map(Some))?;
+
+    // Attempt to parse the host as an IPv4 address.
+    if let Ok(ipv4) = host.parse::<Ipv4Addr>() {
+        return Ok(ParseHostOutput { scheme, host: Host::Ipv4(ipv4), port });
+    }
+
+    // Attempt to parse the host as an IPv6 address enclosed in square brackets.
+    if input.starts_with('[') && input.ends_with(']') {
+        let ipv6 = &input[1..input.len() - 1];
+        if let Ok(ipv6) = ipv6.parse::<Ipv6Addr>() {
+            return Ok(ParseHostOutput { scheme, host: Host::Ipv6(ipv6), port });
+        }
+    }
+    // Attempt to parse the host as an IPv6 address.
+    if let Ok(ipv6) = input.parse::<Ipv6Addr>() {
+        return Ok(ParseHostOutput { scheme, host: Host::Ipv6(ipv6), port });
+    }
+
+    // Attempt to parse the host as a hostname.
+    if host.chars().all(|c| c.is_ascii_alphanumeric() || c == '-') {
+        return Ok(ParseHostOutput { scheme, host: Host::Hostname(host), port });
+    }
+
+    // Attempt to parse the host as a domain.
+    let does_not_start_with_dot = !host.starts_with('.');
+    let does_not_end_with_dot = !host.ends_with('.');
+    let has_at_least_one_dot = host.contains('.');
+    let dots_are_separated_by_valid_chars = host.split('.').all(|part| {
+        let part_does_not_start_with_hyphen = !part.starts_with('-');
+        let part_does_not_end_with_hyphen = !part.ends_with('-');
+        part_does_not_start_with_hyphen && part_does_not_end_with_hyphen && part.chars().all(|c| c.is_ascii_alphanumeric() || c == '-')
+    });
+    let does_not_start_with_hyphen = !host.starts_with('-');
+    let does_not_end_with_hyphen = !host.ends_with('-');
+    let has_atleast_one_hyphen = host.contains('-');
+    let hyphens_are_separated_by_valid_chars =
+        has_atleast_one_hyphen.then(|| host.split('-').all(|part| part.chars().all(|c| c.is_ascii_alphanumeric())));
+    let tld = host.split('.').last();
+    // Prevents e.g. numbers being used as TLDs (which in turn prevents e.g. mistakes in IPv4 addresses as being detected as a domain).
+    let tld_exists_and_is_not_number = tld
+        .map(|tld| {
+            let tld_is_not_number = tld.parse::<i32>().is_err();
+            tld_is_not_number
+        })
+        .unwrap_or(false);
+
+    if does_not_start_with_dot
+        && does_not_end_with_dot
+        && has_at_least_one_dot
+        && dots_are_separated_by_valid_chars
+        && does_not_start_with_hyphen
+        && does_not_end_with_hyphen
+        && hyphens_are_separated_by_valid_chars.unwrap_or(true)
+        && tld_exists_and_is_not_number
+    {
+        return Ok(ParseHostOutput { scheme, host: Host::Domain(host), port });
+    }
+
+    Err(ParseHostError::InvalidInput)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn parse_host_ip_v4_only_loopback() {
+        let input = "127.0.0.1";
+        let output = parse_host(input).unwrap();
+        assert_eq!(output.scheme, None);
+        assert_eq!(output.host, Host::Ipv4(Ipv4Addr::new(127, 0, 0, 1)));
+        assert_eq!(output.port, None);
+    }
+
+    #[test]
+    fn parse_host_ip_v4_only_invalid() {
+        let input = "127.0.0.256";
+        let output = parse_host(input);
+        assert!(output.is_err());
+        let err = output.unwrap_err();
+        assert_eq!(err, ParseHostError::InvalidInput);
+    }
+
+    #[test]
+    fn parse_host_ip_v4_with_port() {
+        let input = "127.0.0.1:8080";
+        let output = parse_host(input).unwrap();
+        assert_eq!(output.scheme, None);
+        assert_eq!(output.host, Host::Ipv4(Ipv4Addr::new(127, 0, 0, 1)));
+        assert_eq!(output.port, Some(8080));
+    }
+
+    #[test]
+    fn parse_host_ip_v4_with_port_and_loopback() {
+        let input = "ws://127.0.0.1:8080";
+        let output = parse_host(input).unwrap();
+        assert_eq!(output.scheme, Some("ws"));
+        assert_eq!(output.host, Host::Ipv4(Ipv4Addr::new(127, 0, 0, 1)));
+        assert_eq!(output.port, Some(8080));
+    }
+
+    #[test]
+    fn parse_host_ip_v6_only_loopback() {
+        let input = "::1";
+        let output = parse_host(input).unwrap();
+        assert_eq!(output.scheme, None);
+        assert_eq!(output.host, Host::Ipv6(Ipv6Addr::new(0, 0, 0, 0, 0, 0, 0, 1)));
+        assert_eq!(output.port, None);
+    }
+
+    #[test]
+    fn parse_host_ip_v6_only_loopback_brackets() {
+        let input = "[::1]";
+        let output = parse_host(input).unwrap();
+        assert_eq!(output.scheme, None);
+        assert_eq!(output.host, Host::Ipv6(Ipv6Addr::new(0, 0, 0, 0, 0, 0, 0, 1)));
+        assert_eq!(output.port, None);
+    }
+}
