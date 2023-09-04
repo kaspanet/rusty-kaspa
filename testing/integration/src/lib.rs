c01c6a49147940776a7be122ed0a5c3200b963d6
Michael Sutton <msutton@cs.huji.ac.il>
2023-09-04 15:47:27 +0300

    rename files/mods

--- testing/integration/src/lib.rs
+++ testing/integration/src/lib.rs
@@ -1,11 +1,11 @@
 pub mod common;
 
 #[cfg(test)]
-pub mod integration_tests;
+pub mod consensus_integration_tests;
 
 #[cfg(test)]
-pub mod pipeline_tests;
+pub mod consensus_pipeline_tests;
 
 #[cfg(test)]
 #[cfg(feature = "devnet-prealloc")]
-pub mod daemon_benchmarks;
+pub mod mempool_benchmarks;
