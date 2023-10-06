9be12580d76afb992f46ab71b3e45dad2c2c0942
coderofstuff <114628839+coderofstuff@users.noreply.github.com>
2023-10-05 21:46:44 -0600

    Update sign/verify to use single object input

--- wallet/core/src/wasm/message.rs
+++ wallet/core/src/wasm/message.rs
@@ -1,29 +1,46 @@
+use crate::imports::*;
 use crate::message::*;
 use kaspa_consensus_wasm::{PrivateKey, PublicKey};
-use wasm_bindgen::prelude::*;
 
-#[wasm_bindgen(js_name = signMessage)]
-pub fn js_sign_message(raw_msg: String, privkey: JsValue) -> String {
-    let mut privkey_bytes = [0u8; 32];
+/// Signs a message with the given private key
+/// @param {object} value - an object containing { message: String, privateKey: String|PrivateKey }
+/// @returns {String} the signature, in hex string format
+#[wasm_bindgen(js_name = signMessage, skip_jsdoc)]
+pub fn js_sign_message(value: JsValue) -> Result<String, Error> {
+    if let Some(object) = Object::try_from(&value) {
+        let private_key = object.get::<PrivateKey>("privateKey")?;
+        let raw_msg = object.get_string("message")?;
+        let mut privkey_bytes = [0u8; 32];
 
-    let private_key = PrivateKey::try_from(privkey).unwrap_throw();
-    privkey_bytes.copy_from_slice(&private_key.secret_bytes());
+        privkey_bytes.copy_from_slice(&private_key.secret_bytes());
 
-    let pm = PersonalMessage(&raw_msg);
+        let pm = PersonalMessage(&raw_msg);
 
-    let sig_result = sign_message(&pm, &privkey_bytes);
-    let sig_vec = sig_result.unwrap_throw();
+        let sig_result = sign_message(&pm, &privkey_bytes);
+        let sig_vec = sig_result.unwrap_throw();
 
-    faster_hex::hex_string(sig_vec.as_slice())
+        Ok(faster_hex::hex_string(sig_vec.as_slice()))
+    } else {
+        Err(Error::custom("Failed to parse input"))
+    }
 }
 
-#[wasm_bindgen(js_name = verifyMessage)]
-pub fn js_verify_message(raw_msg: String, signature: String, pubkey: JsValue) -> bool {
-    let public_key = PublicKey::try_from(pubkey).unwrap_throw();
+/// Verifies with a public key the signature of the given message
+/// @param {object} value - an object containing { message: String, signature: String, publicKey: String|PublicKey }
+/// @returns {bool} true if the signature can be verified with the given public key and message, false otherwise
+#[wasm_bindgen(js_name = verifyMessage, skip_jsdoc)]
+pub fn js_verify_message(value: JsValue) -> Result<bool, Error> {
+    if let Some(object) = Object::try_from(&value) {
+        let public_key = object.get::<PublicKey>("publicKey")?;
+        let raw_msg = object.get_string("message")?;
+        let signature = object.get_string("signature")?;
 
-    let pm = PersonalMessage(&raw_msg);
-    let mut signature_bytes = [0u8; 64];
-    faster_hex::hex_decode(signature.as_bytes(), &mut signature_bytes).unwrap_throw();
+        let pm = PersonalMessage(&raw_msg);
+        let mut signature_bytes = [0u8; 64];
+        faster_hex::hex_decode(signature.as_bytes(), &mut signature_bytes).unwrap_throw();
 
-    verify_message(&pm, &signature_bytes.to_vec(), &public_key.into()).is_ok()
+        Ok(verify_message(&pm, &signature_bytes.to_vec(), &public_key.into()).is_ok())
+    } else {
+        Err(Error::custom("Failed to parse input"))
+    }
 }
--- wasm/nodejs/message_signing.js
+++ wasm/nodejs/message_signing.js
@@ -12,12 +12,12 @@ let message = 'Hello Kaspa!';
 let privkey = 'B7E151628AED2A6ABF7158809CF4F3C762E7160F38B4DA56A784D9045190CFEF';
 let pubkey = 'DFF1D77F2A671C5F36183726DB2341BE58FEAE1DA2DECED843240F7B502BA659';
 
-function runDemo(message, privkey, pubkey) {
-    let signature = signMessage(message, privkey);
+function runDemo(message, privateKey, publicKey) {
+    let signature = signMessage({message, privateKey});
 
     console.info(`Message: ${message} => Signature: ${signature}`);
 
-    if (verifyMessage(message, signature, pubkey)) {
+    if (verifyMessage({message, signature, publicKey})) {
         console.info('Signature verified!');
     } else {
         console.info('Signature is invalid!');
