660bfbf3374524347e451fc4939a65f5e0fdb94b
max143672 <biryukov.maxim.vladimirovich@yandex.ru>
2023-10-07 22:11:11 +0400

    tmp htlc impl

--- crypto/txscript/src/standard/multisig.rs
+++ crypto/txscript/src/standard/multisig.rs
@@ -70,6 +70,8 @@ pub fn multisig_redeem_script_ecdsa(pub_keys: impl Iterator<Item = impl Borrow<[
 #[cfg(test)]
 mod tests {
     use super::*;
+    use crate::opcodes::codes::*;
+    use crate::script_builder::ScriptBuilderResult;
     use crate::{caches::Cache, opcodes::codes::OpData65, pay_to_script_hash_script, TxScriptEngine};
     use core::str::FromStr;
     use kaspa_consensus_core::{
@@ -82,6 +84,7 @@ mod tests {
     };
     use rand::thread_rng;
     use secp256k1::KeyPair;
+    use sha2::{Digest, Sha256};
     use std::{iter, iter::empty};
 
     struct Input {
@@ -293,5 +296,133 @@ mod tests {
             false,
             true,
         );
+    }
+
+    fn htlc_redeem_script(receiver_pubkey: &[u8], sender_pubkey: &[u8], hash: &[u8], locktime: u64) -> ScriptBuilderResult<Vec<u8>> {
+        let mut builder = ScriptBuilder::new();
+        builder
+            // withdraw branch
+            .add_op(OpIf)?
+            .add_op(OpSHA256)?
+            .add_data(hash)?
+            .add_op(OpEqualVerify)?
+            .add_op(OpDup)?
+            .add_data(receiver_pubkey)?
+            .add_op(OpEqualVerify)?
+            .add_op(OpCheckSig)?
+            // refund branch
+            .add_op(OpElse)?
+            .add_lock_time(locktime)?
+            .add_op(OpCheckLockTimeVerify)?
+            .add_op(OpDup)?
+            .add_data(sender_pubkey)?
+            .add_op(OpEqualVerify)?
+            .add_op(OpCheckSig)?
+            // end
+            .add_op(OpEndIf)?;
+
+        Ok(builder.drain())
+    }
+
+    #[test]
+    fn test_htlc() {
+        let [receiver, sender, ..] = kp();
+
+        let mut hasher = Sha256::new();
+        hasher.update(b"hello world");
+        let result = hasher.finalize();
+        let hash = &result[..];
+
+        let script = htlc_redeem_script(
+            receiver.x_only_public_key().0.serialize().as_slice(),
+            sender.x_only_public_key().0.serialize().as_slice(),
+            hash,
+            100,
+        )
+        .unwrap();
+
+        // Taken from: d839d29b549469d0f9a23e51febe68d4084967a6a477868b511a5a8d88c5ae06
+        let prev_tx_id = TransactionId::from_str("63020db736215f8b1105a9281f7bcbb6473d965ecc45bb2fb5da59bd35e6ff84").unwrap();
+
+        let tx = Transaction::new(
+            0,
+            vec![TransactionInput {
+                previous_outpoint: TransactionOutpoint { transaction_id: prev_tx_id, index: 0 },
+                signature_script: vec![],
+                sequence: 101,
+                sig_op_count: 4,
+            }],
+            vec![],
+            101,
+            SubnetworkId::from_bytes([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]),
+            0,
+            vec![],
+        );
+
+        let entries = vec![UtxoEntry {
+            amount: 12793000000000,
+            script_public_key: pay_to_script_hash_script(&script),
+            block_daa_score: 36151168,
+            is_coinbase: false,
+        }];
+
+        // check witdraw
+        {
+            let mut tx = MutableTransaction::with_entries(tx.clone(), entries.clone());
+            let mut reused_values = SigHashReusedValues::new();
+            let sig_hash = calc_schnorr_signature_hash(&tx.as_verifiable(), 0, SIG_HASH_ALL, &mut reused_values);
+            let msg = secp256k1::Message::from_slice(sig_hash.as_bytes().as_slice()).unwrap();
+
+            let sig = receiver.sign_schnorr(msg);
+            let mut signature = Vec::new();
+            signature.extend_from_slice(sig.as_ref().as_slice());
+            signature.push(SIG_HASH_ALL.to_u8());
+
+            let mut builder = ScriptBuilder::new();
+            builder.add_data(&signature).unwrap();
+            builder.add_data(receiver.x_only_public_key().0.serialize().as_slice()).unwrap();
+            builder.add_data(b"hello world").unwrap();
+            builder.add_op(OpTrue).unwrap();
+            builder.add_data(&script).unwrap();
+            {
+                tx.tx.inputs[0].signature_script = builder.drain();
+            }
+
+            let tx = tx.as_verifiable();
+            let (input, entry) = tx.populated_inputs().next().unwrap();
+
+            let cache = Cache::new(10_000);
+            let mut engine = TxScriptEngine::from_transaction_input(&tx, input, 0, entry, &mut reused_values, &cache).unwrap();
+            assert_eq!(engine.execute().is_ok(), true);
+        }
+
+        // check refund
+        {
+            let mut tx = MutableTransaction::with_entries(tx, entries);
+            let mut reused_values = SigHashReusedValues::new();
+            let sig_hash = calc_schnorr_signature_hash(&tx.as_verifiable(), 0, SIG_HASH_ALL, &mut reused_values);
+            let msg = secp256k1::Message::from_slice(sig_hash.as_bytes().as_slice()).unwrap();
+
+            let sig = sender.sign_schnorr(msg);
+            let mut signature = Vec::new();
+            signature.extend_from_slice(sig.as_ref().as_slice());
+            signature.push(SIG_HASH_ALL.to_u8());
+
+            let mut builder = ScriptBuilder::new();
+            builder.add_data(&signature).unwrap();
+            builder.add_data(sender.x_only_public_key().0.serialize().as_slice()).unwrap();
+            builder.add_op(OpFalse).unwrap();
+            builder.add_data(&script).unwrap();
+            {
+                tx.tx.inputs[0].signature_script = builder.drain();
+            }
+
+            let tx = tx.as_verifiable();
+            let (input, entry) = tx.populated_inputs().next().unwrap();
+
+            let cache = Cache::new(10_000);
+            let mut engine = TxScriptEngine::from_transaction_input(&tx, input, 0, entry, &mut reused_values, &cache).unwrap();
+            assert_eq!(engine.execute().is_ok(), true);
+        }
     }
 }
