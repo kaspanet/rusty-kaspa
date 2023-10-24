2225985add8fd1071b87f9644b0119fadd71eca5
max143672 <biryukov.maxim.vladimirovich@yandex.ru>
2023-10-24 16:45:25 +0400

    Add cleanup step to core and task runtime
    This commit introduces a cleanup phase by clearing services in both core and task runtime. This is done to ensure that all services are dropped and properly disposed of after being used. For the task runtime, this cleanup happens after the async-runtime worker stops. For the core, cleanup occurs once it's shut down. This step further advances the robustness of our termination handling.

--- core/src/core.rs
+++ core/src/core.rs
@@ -54,6 +54,9 @@ impl Core {
             }
         }
 
+        // Drop all services and cleanup
+        self.services.lock().unwrap().clear();
+
         trace!("... core is shut down");
     }
 }
