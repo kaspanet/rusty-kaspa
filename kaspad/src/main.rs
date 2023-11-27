45348cca950c2edd49779cf5c61b6d8aec1691b1
aspect <anton.yemelyanov@gmail.com>
2023-11-27 16:23:41 +0200

    Initialize FD limits on MacOS and Linux platforms at startup (#333)
    * Attempt to initialize OS FD limits during the kaspad startup sequence.
    
    * cleanup
    
    * Refactor to use setmaxstdio on windows, remove hard limit and increase DEFAULT and DESIRED limit constants.
    
    * relocate FD limit constants to daemon so that they are available as a part of the kaspad_lib crate (this way if these values are changed, they will propagate downstream)
    
    * rename fd_budget fn to try_set_fd_limit
    
    * fix typo (1024->4096)
    
    * change 4096 to 4*1024
    
    * lints

--- kaspad/src/main.rs
+++ kaspad/src/main.rs
@@ -6,7 +6,10 @@ use std::sync::Arc;
 
 use kaspa_core::{info, signals::Signals};
 use kaspa_utils::fd_budget;
-use kaspad_lib::{args::parse_args, daemon::create_core};
+use kaspad_lib::{
+    args::parse_args,
+    daemon::{create_core, DESIRED_DAEMON_SOFT_FD_LIMIT, MINIMUM_DAEMON_SOFT_FD_LIMIT},
+};
 
 #[cfg(feature = "heap")]
 #[global_allocator]
@@ -17,6 +20,22 @@ pub fn main() {
     let _profiler = dhat::Profiler::builder().file_name("kaspad-heap.json").build();
 
     let args = parse_args();
+
+    match fd_budget::try_set_fd_limit(DESIRED_DAEMON_SOFT_FD_LIMIT) {
+        Ok(limit) => {
+            if limit < MINIMUM_DAEMON_SOFT_FD_LIMIT {
+                println!("Current OS file descriptor limit (soft FD limit) is set to {limit}");
+                println!("The kaspad node requires a setting of at least {DESIRED_DAEMON_SOFT_FD_LIMIT} to operate properly.");
+                println!("Please increase the limits using the following command:");
+                println!("ulimit -n {DESIRED_DAEMON_SOFT_FD_LIMIT}");
+            }
+        }
+        Err(err) => {
+            println!("Unable to initialize the necessary OS file descriptor limit (soft FD limit) to: {}", err);
+            println!("The kaspad node requires a setting of at least {DESIRED_DAEMON_SOFT_FD_LIMIT} to operate properly.");
+        }
+    }
+
     let fd_total_budget = fd_budget::limit() - args.rpc_max_clients as i32 - args.inbound_limit as i32 - args.outbound_target as i32;
     let (core, _) = create_core(args, fd_total_budget);
 
