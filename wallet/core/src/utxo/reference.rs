f1f87ed81881e3fb8d247e5bcc4e26d2fbc9d3a4
Anton Yemelyanov <anton.yemelyanov@gmail.com>
2023-12-10 21:16:07 +0200

    wallet - refactor maturity handling

--- wallet/core/src/utxo/reference.rs
+++ wallet/core/src/utxo/reference.rs
@@ -15,7 +15,17 @@ pub enum Maturity {
     Pending,
     /// UTXO that has reached [`UTXO_MATURITY_PERIOD_COINBASE_TRANSACTION_DAA`] or
     /// [`UTXO_MATURITY_PERIOD_USER_TRANSACTION_DAA`] respectively.
-    Mature,
+    Confirmed,
+}
+
+impl std::fmt::Display for Maturity {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Maturity::Stasis => write!(f, "stasis"),
+            Maturity::Pending => write!(f, "pending"),
+            Maturity::Confirmed => write!(f, "confirmed"),
+        }
+    }
 }
 
 pub trait UtxoEntryReferenceExtension {
@@ -32,12 +42,12 @@ impl UtxoEntryReferenceExtension for UtxoEntryReference {
             {
                 Maturity::Pending
             } else {
-                Maturity::Mature
+                Maturity::Confirmed
             }
         } else if self.block_daa_score() + UTXO_MATURITY_PERIOD_USER_TRANSACTION_DAA.load(Ordering::SeqCst) > current_daa_score {
             Maturity::Pending
         } else {
-            Maturity::Mature
+            Maturity::Confirmed
         }
     }
 
@@ -45,7 +55,7 @@ impl UtxoEntryReferenceExtension for UtxoEntryReference {
         match self.maturity(current_daa_score) {
             Maturity::Pending => Balance::new(0, self.amount(), self.amount(), 0, 1, 0),
             Maturity::Stasis => Balance::new(0, 0, 0, 0, 0, 1),
-            Maturity::Mature => Balance::new(self.amount(), 0, 0, 1, 0, 0),
+            Maturity::Confirmed => Balance::new(self.amount(), 0, 0, 1, 0, 0),
         }
     }
 }
