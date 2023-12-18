b824cebe46cb7482c1f3b016c21602c55573a9e6
Michael Sutton <msutton@cs.huji.ac.il>
2023-12-18 23:00:03 +0000

    updated sync gap comment

--- protocol/flows/src/v5/blockrelay/flow.rs
+++ protocol/flows/src/v5/blockrelay/flow.rs
@@ -253,8 +253,11 @@ impl HandleRelayInvsFlow {
             let hash = block.hash();
             match self.ctx.add_orphan(consensus, block).await {
                 // There is a sync gap between consensus and the orphan pool, meaning that consensus might have indicated
-                // that this block is orphan, but by the time it got to the orphan pool we discovered it is no longer so.
-                // We signal this to the caller by returning false, triggering a consensus processing retry
+                // that this block is orphan, but by the time it got to the orphan pool we discovered it no longer has missing roots.
+                // We signal this to the caller by returning false, triggering a consensus processing retry.
+                // Note that no roots means it is still possible there is a known orphan ancestor in the orphan pool. However
+                // we should still retry consensus in this case because the ancestor might have been queued to consensus
+                // already and consensus handles dependencies with improved (pipeline) concurrency and overlapping
                 Some(OrphanOutput::NoRoots) => return Ok(false),
                 Some(OrphanOutput::Roots(roots)) => self.enqueue_orphan_roots(hash, roots, known_within_range),
                 None | Some(OrphanOutput::Unknown) => {}
