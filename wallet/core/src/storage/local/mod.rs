d2628d8d417e72b47498e1b1b087e72c46174422
Anton Yemelyanov <anton.yemelyanov@gmail.com>
2023-12-24 17:36:33 +0200

    make framework init functions unsafe

--- wallet/core/src/storage/local/mod.rs
+++ wallet/core/src/storage/local/mod.rs
@@ -66,16 +66,56 @@ pub fn default_settings_file() -> &'static str {
 /// NOTE: This function will create a folder if it
 /// doesn't exist. This function will have no effect
 /// if invoked in the browser environment.
-/// @param {String} folder - the path to the storage folder
-#[wasm_bindgen(js_name = setDefaultStorageFolder, skip_jsdoc)]
-pub fn set_default_storage_folder(folder: String) -> Result<()> {
+///
+/// # Safety
+///
+/// This function is unsafe because it is setting a static
+/// mut variable, meaning this function is not thread-safe.
+/// However the function must be used before any other
+/// wallet operations are performed. You must not change
+/// the default storage folder once the wallet has been
+/// initialized.
+///
+pub unsafe fn set_default_storage_folder(folder: String) -> Result<()> {
     create_dir_all_sync(&folder).map_err(|err| Error::custom(format!("Failed to create storage folder: {err}")))?;
+    DEFAULT_STORAGE_FOLDER = Some(folder);
+    Ok(())
+}
+
+/// Set a custom storage folder for the wallet SDK
+/// subsystem.  Encrypted wallet files and transaction
+/// data will be stored in this folder. If not set
+/// the storage folder will default to `~/.kaspa`
+/// (note that the folder is hidden).
+///
+/// This must be called before using any other wallet
+/// SDK functions.
+///
+/// NOTE: This function will create a folder if it
+/// doesn't exist. This function will have no effect
+/// if invoked in the browser environment.
+///
+/// @param {String} folder - the path to the storage folder
+///
+#[wasm_bindgen(js_name = setDefaultStorageFolder, skip_jsdoc)]
+pub fn js_set_default_storage_folder(folder: String) -> Result<()> {
     // SAFETY: This is unsafe because we are setting a static mut variable
     // meaning this function is not thread-safe. However the function
     // must be used before any other wallet operations are performed.
-    unsafe {
-        DEFAULT_STORAGE_FOLDER = Some(folder);
-    }
+    unsafe { set_default_storage_folder(folder) }
+}
+
+/// Set the name of the default wallet file name
+/// or the `localStorage` key.  If `Wallet::open`
+/// is called without a wallet file name, this name
+/// will be used.  Please note that this name
+/// will be suffixed with `.wallet` suffix.
+///
+/// This function should be called before using any
+/// other wallet SDK functions.
+///
+pub unsafe fn set_default_wallet_file(folder: String) -> Result<()> {
+    DEFAULT_WALLET_FILE = Some(folder);
     Ok(())
 }
 
@@ -89,8 +129,11 @@ pub fn set_default_storage_folder(folder: String) -> Result<()> {
 /// other wallet SDK functions.
 ///
 /// @param {String} - the name to the wallet file or key.
+///
 #[wasm_bindgen(js_name = setDefaultWalletFile, skip_jsdoc)]
-pub fn set_default_wallet_file(folder: String) -> Result<()> {
+pub fn js_set_default_wallet_file(folder: String) -> Result<()> {
+    // SAFETY: This is unsafe because we are setting a static mut variable
+    // meaning this function is not thread-safe.
     unsafe {
         DEFAULT_WALLET_FILE = Some(folder);
     }
