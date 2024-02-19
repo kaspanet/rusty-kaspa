437f09920a4f0e3a176625782ea7074be3bc4cf0
Anton Yemelyanov <anton.yemelyanov@gmail.com>
2024-02-19 12:11:32 +0200

    WASM32 rename packages

--- wasm/package-sizes.js
+++ wasm/package-sizes.js
@@ -1,10 +1,10 @@
 window.packageSizes = {
+  "kaspa": "8.41 MB",
   "kaspa-core": "6.51 MB",
   "kaspa-core-dev": "26.50 MB",
+  "kaspa-dev": "32.93 MB",
   "kaspa-keygen": "1.62 MB",
   "kaspa-keygen-dev": "3.79 MB",
   "kaspa-rpc": "2.91 MB",
-  "kaspa-rpc-dev": "15.18 MB",
-  "kaspa-wallet": "8.41 MB",
-  "kaspa-wallet-dev": "32.93 MB"
+  "kaspa-rpc-dev": "15.18 MB"
 };
