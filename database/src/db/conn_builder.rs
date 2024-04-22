43844040a4b4d7b0605cc35b82c8078c4dc913f3
max143672 <biryukov.maxim.vladimirovich@yandex.ru>
2024-04-22 20:02:51 +0400

    pass rocksdb cache size parameter via cli args to db connector

--- database/src/db/conn_builder.rs
+++ database/src/db/conn_builder.rs
@@ -10,16 +10,17 @@ const GB: usize = 1024 * MB;
 pub struct Unspecified;
 
 #[derive(Debug)]
-pub struct ConnBuilder<Path, const STATS_ENABLED: bool, StatsPeriod, FDLimit> {
+pub struct ConnBuilder<Path, const STATS_ENABLED: bool, StatsPeriod, FDLimit, CacheSize> {
     db_path: Path,
     create_if_missing: bool,
     parallelism: usize,
     files_limit: FDLimit,
     mem_budget: usize,
     stats_period: StatsPeriod,
+    cache_size: CacheSize,
 }
 
-impl Default for ConnBuilder<Unspecified, true, Unspecified, Unspecified> {
+impl Default for ConnBuilder<Unspecified, true, Unspecified, Unspecified, Unspecified> {
     fn default() -> Self {
         ConnBuilder {
             db_path: Unspecified,
@@ -28,12 +29,15 @@ impl Default for ConnBuilder<Unspecified, true, Unspecified, Unspecified> {
             mem_budget: 64 * 1024 * 1024,
             stats_period: Unspecified,
             files_limit: Unspecified,
+            cache_size: Unspecified,
         }
     }
 }
 
-impl<Path, const STATS_ENABLED: bool, StatsPeriod, FDLimit> ConnBuilder<Path, STATS_ENABLED, StatsPeriod, FDLimit> {
-    pub fn with_db_path(self, db_path: PathBuf) -> ConnBuilder<PathBuf, STATS_ENABLED, StatsPeriod, FDLimit> {
+impl<Path, const STATS_ENABLED: bool, StatsPeriod, FDLimit, CacheSize>
+    ConnBuilder<Path, STATS_ENABLED, StatsPeriod, FDLimit, CacheSize>
+{
+    pub fn with_db_path(self, db_path: PathBuf) -> ConnBuilder<PathBuf, STATS_ENABLED, StatsPeriod, FDLimit, CacheSize> {
         ConnBuilder {
             db_path,
             files_limit: self.files_limit,
@@ -41,18 +45,19 @@ impl<Path, const STATS_ENABLED: bool, StatsPeriod, FDLimit> ConnBuilder<Path, ST
             parallelism: self.parallelism,
             mem_budget: self.mem_budget,
             stats_period: self.stats_period,
+            cache_size: self.cache_size,
         }
     }
-    pub fn with_create_if_missing(self, create_if_missing: bool) -> ConnBuilder<Path, STATS_ENABLED, StatsPeriod, FDLimit> {
+    pub fn with_create_if_missing(self, create_if_missing: bool) -> ConnBuilder<Path, STATS_ENABLED, StatsPeriod, FDLimit, CacheSize> {
         ConnBuilder { create_if_missing, ..self }
     }
-    pub fn with_parallelism(self, parallelism: impl Into<usize>) -> ConnBuilder<Path, STATS_ENABLED, StatsPeriod, FDLimit> {
+    pub fn with_parallelism(self, parallelism: impl Into<usize>) -> ConnBuilder<Path, STATS_ENABLED, StatsPeriod, FDLimit, CacheSize> {
         ConnBuilder { parallelism: parallelism.into(), ..self }
     }
-    pub fn with_mem_budget(self, mem_budget: impl Into<usize>) -> ConnBuilder<Path, STATS_ENABLED, StatsPeriod, FDLimit> {
+    pub fn with_mem_budget(self, mem_budget: impl Into<usize>) -> ConnBuilder<Path, STATS_ENABLED, StatsPeriod, FDLimit, CacheSize> {
         ConnBuilder { mem_budget: mem_budget.into(), ..self }
     }
-    pub fn with_files_limit(self, files_limit: impl Into<i32>) -> ConnBuilder<Path, STATS_ENABLED, StatsPeriod, i32> {
+    pub fn with_files_limit(self, files_limit: impl Into<i32>) -> ConnBuilder<Path, STATS_ENABLED, StatsPeriod, i32, CacheSize> {
         ConnBuilder {
             db_path: self.db_path,
             files_limit: files_limit.into(),
@@ -60,9 +65,10 @@ impl<Path, const STATS_ENABLED: bool, StatsPeriod, FDLimit> ConnBuilder<Path, ST
             parallelism: self.parallelism,
             mem_budget: self.mem_budget,
             stats_period: self.stats_period,
+            cache_size: self.cache_size,
         }
     }
-    pub fn disable_stats(self) -> ConnBuilder<Path, false, Unspecified, FDLimit> {
+    pub fn disable_stats(self) -> ConnBuilder<Path, false, Unspecified, FDLimit, CacheSize> {
         ConnBuilder {
             db_path: self.db_path,
             create_if_missing: self.create_if_missing,
@@ -70,12 +76,13 @@ impl<Path, const STATS_ENABLED: bool, StatsPeriod, FDLimit> ConnBuilder<Path, ST
             files_limit: self.files_limit,
             mem_budget: self.mem_budget,
             stats_period: Unspecified,
+            cache_size: self.cache_size,
         }
     }
 }
 
-impl<Path, const STATS_ENABLED: bool, FDLimit> ConnBuilder<Path, STATS_ENABLED, Unspecified, FDLimit> {
-    pub fn enable_stats(self) -> ConnBuilder<Path, true, Unspecified, FDLimit> {
+impl<Path, const STATS_ENABLED: bool, FDLimit, CacheSize> ConnBuilder<Path, STATS_ENABLED, Unspecified, FDLimit, CacheSize> {
+    pub fn enable_stats(self) -> ConnBuilder<Path, true, Unspecified, FDLimit, CacheSize> {
         ConnBuilder {
             db_path: self.db_path,
             create_if_missing: self.create_if_missing,
@@ -83,12 +90,13 @@ impl<Path, const STATS_ENABLED: bool, FDLimit> ConnBuilder<Path, STATS_ENABLED, 
             files_limit: self.files_limit,
             mem_budget: self.mem_budget,
             stats_period: self.stats_period,
+            cache_size: self.cache_size,
         }
     }
 }
 
-impl<Path, StatsPeriod, FDLimit> ConnBuilder<Path, true, StatsPeriod, FDLimit> {
-    pub fn with_stats_period(self, stats_period: impl Into<u32>) -> ConnBuilder<Path, true, u32, FDLimit> {
+impl<Path, StatsPeriod, FDLimit, CacheSize> ConnBuilder<Path, true, StatsPeriod, FDLimit, CacheSize> {
+    pub fn with_stats_period(self, stats_period: impl Into<u32>) -> ConnBuilder<Path, true, u32, FDLimit, CacheSize> {
         ConnBuilder {
             db_path: self.db_path,
             create_if_missing: self.create_if_missing,
@@ -96,12 +104,25 @@ impl<Path, StatsPeriod, FDLimit> ConnBuilder<Path, true, StatsPeriod, FDLimit> {
             files_limit: self.files_limit,
             mem_budget: self.mem_budget,
             stats_period: stats_period.into(),
+            cache_size: self.cache_size,
         }
     }
 }
 
-// used ChatGPT to modify the section below to change the rocksdb
-// block size to 128KB
+impl<Path, StatsPeriod, FDLimit, CacheSize> ConnBuilder<Path, true, StatsPeriod, FDLimit, CacheSize> {
+    pub fn with_cache_size(self, cache_size: usize) -> ConnBuilder<Path, true, StatsPeriod, FDLimit, usize> {
+        ConnBuilder {
+            db_path: self.db_path,
+            create_if_missing: self.create_if_missing,
+            parallelism: self.parallelism,
+            files_limit: self.files_limit,
+            mem_budget: self.mem_budget,
+            stats_period: self.stats_period,
+            cache_size,
+        }
+    }
+}
+
 macro_rules! default_opts {
     ($self: expr) => {{
         let mut opts = rocksdb::Options::default();
@@ -112,10 +133,10 @@ macro_rules! default_opts {
         {
             // based on optimized by default
             opts.set_max_background_jobs((available_parallelism().unwrap().get() / 2) as i32);
-            opts.set_compaction_readahead_size(2097152);
+            opts.set_compaction_readahead_size(2 * MB);
             opts.set_level_zero_stop_writes_trigger(36);
             opts.set_level_zero_slowdown_writes_trigger(20);
-            opts.set_max_compaction_bytes(209715200);
+            opts.set_max_compaction_bytes(2 * MB as u64 * 100);
         }
         {
             let buffer_size = 32usize * MB;
@@ -149,43 +170,29 @@ macro_rules! default_opts {
             ]);
             opts.set_level_compaction_dynamic_level_bytes(true); // default option since 8.4 https://github.com/facebook/rocksdb/wiki/Leveled-Compaction#level_compaction_dynamic_level_bytes-is-true-recommended-default-since-version-84
 
-            let mut b_opts = BlockBasedOptions::default();
-            b_opts.set_bloom_filter(4.9, true); // increases ram, trade-off, needs to be tested
-            b_opts.set_block_size(128 * KB); // Callidon tests shows that this size increases block processing during the sync significantly
-
-            // todo remove me, should be properly passed via cli
-            if let Some(cache_size) = std::env::var("ROCKSDB_CACHE_SIZE").ok().and_then(|str| str.parse().ok()) {
-                println!("`ROCKSDB_CACHE_SIZE` is settled to {cache_size}");
-                let cache = Cache::new_lru_cache(cache_size);
-                b_opts.set_block_cache(&cache);
-            }
-
-            opts.set_block_based_table_factory(&b_opts);
+            // Create BlockBasedOptions and set block size
+            let mut block_opts = rocksdb::BlockBasedOptions::default();
+            block_opts.set_bloom_filter(4.9, true); // increases ram, trade-off, needs to be tested
+            block_opts.set_block_size(128 * KB);
+            $self.cache_size.apply_cache_size(&mut block_opts);
+            opts.set_block_based_table_factory(&block_opts);
         }
         let guard = kaspa_utils::fd_budget::acquire_guard($self.files_limit)?;
-
-        // Create BlockBasedOptions and set block size
-        let mut block_opts = rocksdb::BlockBasedOptions::default();
-        block_opts.set_block_size(128 * 1024);
-        opts.set_block_based_table_factory(&block_opts);
-
         opts.set_max_open_files($self.files_limit);
         opts.create_if_missing($self.create_if_missing);
         Ok((opts, guard))
     }};
-}  
+}
 
-impl ConnBuilder<PathBuf, false, Unspecified, i32> {
+impl<T: ApplyCacheSize> ConnBuilder<PathBuf, false, Unspecified, i32, T> {
     pub fn build(self) -> Result<Arc<DB>, kaspa_utils::fd_budget::Error> {
-        let (mut opts, guard) = default_opts!(self)?;
-        opts.set_report_bg_io_stats(true);
-        opts.enable_statistics();
+        let (opts, guard) = default_opts!(self)?;
         let db = Arc::new(DB::new(<DBWithThreadMode<MultiThreaded>>::open(&opts, self.db_path.to_str().unwrap()).unwrap(), guard));
         Ok(db)
     }
 }
 
-impl ConnBuilder<PathBuf, true, Unspecified, i32> {
+impl<T: ApplyCacheSize> ConnBuilder<PathBuf, true, Unspecified, i32, T> {
     pub fn build(self) -> Result<Arc<DB>, kaspa_utils::fd_budget::Error> {
         let (mut opts, guard) = default_opts!(self)?;
         opts.enable_statistics();
@@ -195,14 +202,28 @@ impl ConnBuilder<PathBuf, true, Unspecified, i32> {
     }
 }
 
-impl ConnBuilder<PathBuf, true, u32, i32> {
+impl<T: ApplyCacheSize> ConnBuilder<PathBuf, true, u32, i32, T> {
     pub fn build(self) -> Result<Arc<DB>, kaspa_utils::fd_budget::Error> {
         let (mut opts, guard) = default_opts!(self)?;
-
         opts.enable_statistics();
         opts.set_report_bg_io_stats(true);
         opts.set_stats_dump_period_sec(self.stats_period);
         let db = Arc::new(DB::new(<DBWithThreadMode<MultiThreaded>>::open(&opts, self.db_path.to_str().unwrap()).unwrap(), guard));
         Ok(db)
     }
+}
+
+pub trait ApplyCacheSize {
+    fn apply_cache_size(&self, options: &mut BlockBasedOptions);
+}
+
+impl ApplyCacheSize for usize {
+    fn apply_cache_size(&self, options: &mut BlockBasedOptions) {
+        let cache = Cache::new_lru_cache(*self);
+        options.set_block_cache(&cache);
+    }
+}
+
+impl ApplyCacheSize for Unspecified {
+    fn apply_cache_size(&self, _options: &mut BlockBasedOptions) {}
 }
