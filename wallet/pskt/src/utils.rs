5a0c8da0dad40031ce74632e18a6f8dcb3e0234c
max143672 <biryukov.maxim.vladimirovich@yandex.ru>
2024-06-14 17:41:22 +0300

    refactor: avoid copy-paste by using recursion

--- wallet/pskt/src/utils.rs
+++ wallet/pskt/src/utils.rs
@@ -1,27 +1,22 @@
 use std::collections::BTreeMap;
 
 // todo optimize without cloning
-pub fn combine_if_no_conflicts<K, V>(mut lhs: BTreeMap<K, V>, mut rhs: BTreeMap<K, V>) -> Result<BTreeMap<K, V>, Error<K, V>>
+pub fn combine_if_no_conflicts<K, V>(mut lhs: BTreeMap<K, V>, rhs: BTreeMap<K, V>) -> Result<BTreeMap<K, V>, Error<K, V>>
 where
     V: Eq + Clone,
     K: Ord + Clone,
 {
-    if lhs.len() < rhs.len() {
-        if let Some((field, lhs, rhs)) =
-            lhs.iter().map(|(k, v)| (k, v, rhs.get(k))).find(|(_, v, rhs_v)| rhs_v.is_some_and(|rv| rv != *v))
+    if lhs.len() > rhs.len() {
+        if let Some((field, rhs, lhs)) =
+            rhs.iter().map(|(k, v)| (k, v, lhs.get(k))).find(|(_, v, rhs_v)| rhs_v.is_some_and(|rv| rv != *v))
         {
-            Err(Error { field: field.clone(), lhs: lhs.clone(), rhs: rhs.unwrap().clone() })
+            Err(Error { field: field.clone(), lhs: lhs.unwrap().clone(), rhs: rhs.clone() })
         } else {
-            rhs.extend(lhs);
-            Ok(rhs)
+            lhs.extend(rhs);
+            Ok(lhs)
         }
-    } else if let Some((field, rhs, lhs)) =
-        rhs.iter().map(|(k, v)| (k, v, lhs.get(k))).find(|(_, v, rhs_v)| rhs_v.is_some_and(|rv| rv != *v))
-    {
-        return Err(Error { field: field.clone(), lhs: lhs.unwrap().clone(), rhs: rhs.clone() });
     } else {
-        lhs.extend(rhs);
-        Ok(lhs)
+        combine_if_no_conflicts(rhs, lhs)
     }
 }
 
