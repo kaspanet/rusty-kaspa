bacfd81a3bc24527c99a0071b84b7eb476a681e7
surinder singh <surinder83singh@gmail.com>
2024-07-21 05:00:37 +0530

    Update phrase.rs (#74)

--- wallet/bip32/src/mnemonic/phrase.rs
+++ wallet/bip32/src/mnemonic/phrase.rs
@@ -23,7 +23,7 @@ pub type Entropy32 = [u8; KEY_SIZE];
 pub type Entropy16 = [u8; 16];
 
 /// Word count for a BIP39 mnemonic phrase. Identifies mnemonic as 12 or 24 word variants.
-#[derive(Default, Clone, Copy, Debug, Serialize, Deserialize, BorshSerialize, BorshDeserialize)]
+#[derive(Default, PartialEq, Eq, Clone, Copy, Debug, Serialize, Deserialize, BorshSerialize, BorshDeserialize)]
 #[serde(rename_all = "kebab-case")]
 pub enum WordCount {
     #[default]
