a40a0db39a077dbe51dd9ea8313658c49334e926
smartgoo <cvallone28@gmail.com>
2024-07-21 05:10:38 -0700

    Resolver for Python wRPC client prototype (#70)
    * wRPC minor cleanup
    
    * resolver scaffolding
    
    * wRPC client failing to init due to resolver network issue
    
    * wRPC client constructor accept Resolver, network id
    
    * network null when using url
    
    * connect options and resolver methods
    
    * lints
    
    * rpc.py example tn11
    
    * set network id on client
    
    * bubble errors where possible
    
    * lint

--- python/examples/rpc.py
+++ python/examples/rpc.py
@@ -3,15 +3,15 @@ import json
 import time
 import os
 
-from kaspa import RpcClient
+from kaspa import RpcClient, Resolver
 
 
 def subscription_callback(event, name, **kwargs):
-    print(f'{name} | {event}')
+    print(f"{name} | {event}")
 
 async def rpc_subscriptions(client):
-    # client.add_event_listener('all', subscription_callback, callback_id=1, kwarg1='Im a kwarg!!')
-    client.add_event_listener('all', subscription_callback, name="all")
+    # client.add_event_listener("all", subscription_callback, callback_id=1, kwarg1="Im a kwarg!!")
+    client.add_event_listener("all", subscription_callback, name="all")
 
     await client.subscribe_virtual_daa_score_changed()
     await client.subscribe_virtual_chain_changed(True)
@@ -20,8 +20,8 @@ async def rpc_subscriptions(client):
 
     await asyncio.sleep(5)
 
-    client.remove_event_listener('all')
-    print('Removed all event listeners. Sleeping for 5 seconds before unsubscribing. Should see nothing print.')
+    client.remove_event_listener("all")
+    print("Removed all event listeners. Sleeping for 5 seconds before unsubscribing. Should see nothing print.")
 
     await asyncio.sleep(5)
 
@@ -38,20 +38,21 @@ async def rpc_calls(client):
     block_dag_info_response = await client.get_block_dag_info()
     print(block_dag_info_response)
 
-    tip_hash = block_dag_info_response['tipHashes'][0]
-    get_block_request = {'hash': tip_hash, 'includeTransactions': True}
+    tip_hash = block_dag_info_response["tipHashes"][0]
+    get_block_request = {"hash": tip_hash, "includeTransactions": True}
     get_block_response = await client.get_block_call(get_block_request)
     print(get_block_response)
 
-    get_balances_by_addresses_request = {'addresses': ['kaspa:qqxn4k5dchwk3m207cmh9ewagzlwwvfesngkc8l90tj44mufcgmujpav8hakt', 'kaspa:qr5ekyld6j4zn0ngennj9nx5gpt3254fzs77ygh6zzkvyy8scmp97de4ln8v5']}
+    get_balances_by_addresses_request = {"addresses": ["kaspa:qqxn4k5dchwk3m207cmh9ewagzlwwvfesngkc8l90tj44mufcgmujpav8hakt", "kaspa:qr5ekyld6j4zn0ngennj9nx5gpt3254fzs77ygh6zzkvyy8scmp97de4ln8v5"]}
     get_balances_by_addresses_response =  await client.get_balances_by_addresses_call(get_balances_by_addresses_request)
     print(get_balances_by_addresses_response)
 
 async def main():
-    rpc_host = os.environ.get("KASPA_RPC_HOST")
-    client = RpcClient(url = f"ws://{rpc_host}:17210")
+    # rpc_host = os.environ.get("KASPA_RPC_HOST")
+    # client = RpcClient(url=f"ws://{rpc_host}:17210")
+    client = RpcClient(resolver=Resolver(), network="testnet", network_suffix=11)
     await client.connect()
-    print(f'Client is connected: {client.is_connected()}')
+    print(f"Client is connected: {client.is_connected()}")
 
     await rpc_calls(client)
     await rpc_subscriptions(client)
