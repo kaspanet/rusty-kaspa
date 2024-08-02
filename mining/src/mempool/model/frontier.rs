cc825eff3a52fde5625cf226efdf64395bd708d4
Michael Sutton <msutton@cs.huji.ac.il>
2024-08-02 12:56:20 +0000

    Test feerate weight queries + an implied fix (change <= to <)

--- mining/src/mempool/model/frontier.rs
+++ mining/src/mempool/model/frontier.rs
@@ -38,7 +38,7 @@ pub mod arg {
             for (i, k) in keys.iter().enumerate() {
                 let w = k.weight();
                 sum += w;
-                if query <= sum {
+                if query < sum {
                     return Some(i);
                 }
             }
@@ -122,8 +122,12 @@ impl Frontier {
         }))
     }
 
-    pub(crate) fn len(&self) -> usize {
+    pub fn len(&self) -> usize {
         self.feerate_order.len()
+    }
+
+    pub fn is_empty(&self) -> bool {
+        self.len() == 0
     }
 }
 
@@ -147,6 +151,10 @@ mod tests {
         Arc::new(Transaction::new(0, vec![input], vec![], 0, SUBNETWORK_ID_NATIVE, 0, vec![]))
     }
 
+    fn build_feerate_key(fee: u64, mass: u64, id: u64) -> FeerateTransactionKey {
+        FeerateTransactionKey::new(fee, mass, generate_unique_tx(id))
+    }
+
     fn stage_two_sampling(container: impl IntoIterator<Item = FeerateTransactionKey>) -> Vec<Transaction> {
         let set = container.into_iter().map(CandidateTransaction::from_key).collect_vec();
         let mut selector = TransactionsSelector::new(Policy::new(500_000), set);
@@ -156,7 +164,7 @@ mod tests {
     #[test]
     pub fn test_two_stage_sampling() {
         let mut rng = thread_rng();
-        let cap = 100_000;
+        let cap = 1000;
         let mut map = HashMap::with_capacity(cap);
         for i in 0..cap as u64 {
             let fee: u64 = if i % (cap as u64 / 100) == 0 { 1000000 } else { rng.gen_range(1..10000) };
@@ -174,6 +182,30 @@ mod tests {
         let stage_one = frontier.sample(&mut rng, 10_000);
         let stage_two = stage_two_sampling(stage_one);
         stage_two.into_iter().map(|k| k.gas).sum::<u64>();
+    }
+
+    #[test]
+    fn test_feerate_weight_queries() {
+        let mut btree: BPlusTreeMap<FeerateTransactionKey, (), FeerateWeight> = BPlusTreeMap::new();
+        let mass = 2000;
+        let fees = [123, 113, 10_000, 1000, 2050, 2048];
+        let mut weights = Vec::with_capacity(fees.len());
+        for (i, fee) in fees.iter().copied().enumerate() {
+            let key = build_feerate_key(fee, mass, i as u64);
+            weights.push(key.weight());
+            btree.insert(key, ());
+        }
+        let fees_weights = fees.into_iter().zip(weights).sorted_by(|(_, w1), (_, w2)| w1.total_cmp(w2)).collect_vec();
+        let eps = 0.000000001;
+        let mut sum = 0.0;
+        for (fee, weight) in fees_weights {
+            let samples = [sum, sum + weight / 2.0, sum + weight - eps];
+            for sample in samples {
+                let key = btree.get_by_argument(sample).unwrap().0;
+                assert_eq!(fee, key.fee);
+            }
+            sum += weight;
+        }
     }
 
     #[test]
