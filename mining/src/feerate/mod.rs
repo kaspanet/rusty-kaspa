e2024f9654358355aa60737bad0da805be2d3573
Michael Sutton <msutton@cs.huji.ac.il>
2024-08-05 14:10:00 +0000

    fee estimator todos

--- mining/src/feerate/mod.rs
+++ mining/src/feerate/mod.rs
@@ -1,4 +1,12 @@
+//! See the accompanying fee_estimation.ipynb Jupyter Notebook which details the reasoning
+//! behind this fee estimator.
+
 use crate::block_template::selector::ALPHA;
+use kaspa_utils::vec::VecExtensions;
+
+/// The current standard minimum feerate (fee/mass = 1.0 is the current standard minimum).
+/// TODO: pass from config
+const MIN_FEERATE: f64 = 1.0;
 
 #[derive(Clone, Copy, Debug)]
 pub struct FeerateBucket {
@@ -8,9 +16,25 @@ pub struct FeerateBucket {
 
 #[derive(Clone, Debug)]
 pub struct FeerateEstimations {
-    pub priority_bucket: FeerateBucket, // TODO (PR): priority = sub-second and at least 1.0 feerate (standard)
-    pub normal_bucket: FeerateBucket,   // TODO (PR): change to a vec with sub-minute guarantee for first value
-    pub low_bucket: FeerateBucket,      // TODO (PR): change to a vec with sub-hour guarantee for first value
+    /// *Top-priority* feerate bucket. Provides an estimation of the feerate required for sub-second DAG inclusion.
+    pub priority_bucket: FeerateBucket,
+
+    /// A vector of *normal* priority feerate values. The first value of this vector is guaranteed to
+    /// provide an estimation for sub-*minute* DAG inclusion. All other values will have shorter estimation
+    /// times than all `low_bucket` values. Therefor by chaining `[priority] | normal | low` and interpolating
+    /// between them, once can compose a complete feerate function on the client side. The API makes an effort
+    /// to sample enough "interesting" points on the feerate-to-time curve, so that the interpolation is meaningful.
+    pub normal_buckets: Vec<FeerateBucket>,
+
+    /// A vector of *low* priority feerate values. The first value of this vector is guaranteed to
+    /// provide an estimation for sub-*hour* DAG inclusion.
+    pub low_buckets: Vec<FeerateBucket>,
+}
+
+impl FeerateEstimations {
+    pub fn ordered_buckets(&self) -> Vec<FeerateBucket> {
+        vec![self.priority_bucket].merge(self.normal_buckets.clone()).merge(self.low_buckets.clone())
+    }
 }
 
 pub struct FeerateEstimatorArgs {
@@ -65,13 +89,13 @@ impl FeerateEstimator {
     }
 
     pub fn calc_estimations(&self) -> FeerateEstimations {
-        let high = self.time_to_feerate(1f64);
-        let low = self.time_to_feerate(3600f64).max(self.quantile(1f64, high, 0.25));
-        let mid = self.time_to_feerate(60f64).max(self.quantile(low, high, 0.5));
+        let high = self.time_to_feerate(1f64).max(MIN_FEERATE);
+        let low = self.time_to_feerate(3600f64).max(MIN_FEERATE).max(self.quantile(1f64, high, 0.25));
+        let mid = self.time_to_feerate(60f64).max(MIN_FEERATE).max(self.quantile(low, high, 0.5));
         FeerateEstimations {
             priority_bucket: FeerateBucket { feerate: high, estimated_seconds: self.feerate_to_time(high) },
-            normal_bucket: FeerateBucket { feerate: mid, estimated_seconds: self.feerate_to_time(mid) },
-            low_bucket: FeerateBucket { feerate: low, estimated_seconds: self.feerate_to_time(low) },
+            normal_buckets: vec![FeerateBucket { feerate: mid, estimated_seconds: self.feerate_to_time(mid) }],
+            low_buckets: vec![FeerateBucket { feerate: low, estimated_seconds: self.feerate_to_time(low) }],
         }
     }
 }
@@ -79,15 +103,27 @@ impl FeerateEstimator {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use itertools::Itertools;
 
     #[test]
     fn test_feerate_estimations() {
         let estimator = FeerateEstimator { total_weight: 1002283.659, inclusion_interval: 0.004f64 };
         let estimations = estimator.calc_estimations();
-        assert!(estimations.low_bucket.feerate <= estimations.normal_bucket.feerate);
-        assert!(estimations.normal_bucket.feerate <= estimations.priority_bucket.feerate);
-        assert!(estimations.low_bucket.estimated_seconds >= estimations.normal_bucket.estimated_seconds);
-        assert!(estimations.normal_bucket.estimated_seconds >= estimations.priority_bucket.estimated_seconds);
+        let buckets = estimations.ordered_buckets();
+        for (i, j) in buckets.into_iter().tuple_windows() {
+            assert!(i.feerate >= j.feerate);
+        }
         dbg!(estimations);
+    }
+
+    #[test]
+    fn test_min_feerate_estimations() {
+        let estimator = FeerateEstimator { total_weight: 0.00659, inclusion_interval: 0.004f64 };
+        let estimations = estimator.calc_estimations();
+        let buckets = estimations.ordered_buckets();
+        assert!(buckets.last().unwrap().feerate >= MIN_FEERATE);
+        for (i, j) in buckets.into_iter().tuple_windows() {
+            assert!(i.feerate >= j.feerate);
+        }
     }
 }
