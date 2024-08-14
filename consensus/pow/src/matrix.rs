6c72d810ea40b76c911f3e4e0caa9001d95b31fe
Slixe <slixeprivate@gmail.com>
2024-08-15 00:44:38 +0200

    matrix: remove unsafe usage for convert_to_float

--- consensus/pow/src/matrix.rs
+++ consensus/pow/src/matrix.rs
@@ -51,16 +51,15 @@ impl Matrix {
 
     #[inline(always)]
     fn convert_to_float(&self) -> [[f64; 64]; 64] {
-        // SAFETY: An uninitialized MaybeUninit is always safe.
-        let mut out: [[MaybeUninit<f64>; 64]; 64] = unsafe { MaybeUninit::uninit().assume_init() };
+        let mut out: [[f64; 64]; 64] = [[Default::default(); 64]; 64];
 
         out.iter_mut().zip(self.0.iter()).for_each(|(out_row, mat_row)| {
             out_row.iter_mut().zip(mat_row).for_each(|(out_element, &element)| {
-                out_element.write(f64::from(element));
+                *out_element = f64::from(element);
             })
         });
-        // SAFETY: The loop above wrote into all indexes.
-        unsafe { std::mem::transmute(out) }
+
+        out
     }
 
     pub fn compute_rank(&self) -> usize {
