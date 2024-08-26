27c95f4f413dd5f48e3ba163358aa79a65aa37c2
Michael Sutton <msutton@cs.huji.ac.il>
2024-08-26 13:58:00 +0300

    A few optimizations related to multi-level relations (#527)
    * parents_builder: optimize the common case for high levels
    
    * delete level relations for all levels below the affiliated proof level for this block
    
    * keep all multi-level parents of the pruning-point-and-anticone roots set
    
    * drop prune guard where possible
    
    * minor
    
    * practically impossible to reach this level (requires a pow hash which is all zeros), but for the sake of good order it should be this way
    
    * comments
    
    * fix `get_roots_of_set` for the ascending chain case + test
    
    * avoid quadratic roots search + rely on header cache
    
    * rollback `get_roots_of_set`

--- consensus/src/processes/reachability/tests/mod.rs
+++ consensus/src/processes/reachability/tests/mod.rs
@@ -105,6 +105,12 @@ impl DagBlock {
     }
 }
 
+impl From<(u64, &[u64])> for DagBlock {
+    fn from(value: (u64, &[u64])) -> Self {
+        Self::new(value.0.into(), value.1.iter().map(|&i| i.into()).collect())
+    }
+}
+
 /// A struct with fluent API to streamline DAG building
 pub struct DagBuilder<'a, T: ReachabilityStore + ?Sized, S: RelationsStore + ChildrenStore + ?Sized> {
     reachability: &'a mut T,
