6d6faade01c2269527fec7d2b4b7d4b0b99ba4af
starkbamse <139136798+starkbamse@users.noreply.github.com>
2024-09-05 15:29:39 +0200

    Use openssl crate

--- musl-toolchain/build.sh
+++ musl-toolchain/build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 PRESET_HASH_FILE="$HOME/x-tools/preset_hash"
+
 # Calculate the hash of the preset file
 CURRENT_PRESET_HASH=$(sha256sum $GITHUB_WORKSPACE/musl-toolchain/preset.sh | awk '{print $1}')
 
@@ -25,12 +26,14 @@ if [ ! -d "$HOME/x-tools" ] || [ ! -f "$PRESET_HASH_FILE" ] || [ "$(cat $PRESET_
   make install
   # Add crosstool-ng to PATH
   export PATH=$HOME/ctng/bin:$PATH
+  
   # Configure and build the musl toolchain
   cd $GITHUB_WORKSPACE/musl-toolchain
 
+  # Set the preset
   source preset.sh
 
-  # Expand mini config
+  # Load toolchainc configuration
   ct-ng $CTNG_PRESET
   
   # Build the toolchain
@@ -45,6 +48,8 @@ if [ ! -d "$HOME/x-tools" ] || [ ! -f "$PRESET_HASH_FILE" ] || [ "$(cat $PRESET_
   if [ $status -eq 0 ]; then
     echo "Build succeeded"
     ls -la $HOME/x-tools
+    # Store the current hash of preset.sh after successful build
+    echo "$CURRENT_PRESET_HASH" > "$PRESET_HASH_FILE"    
   else
     echo "Build failed, here's the log:"
     cat .config
@@ -58,35 +63,22 @@ export CXX=$HOME/x-tools/$CTNG_PRESET/bin/$CTNG_PRESET-g++
 export LD=$HOME/x-tools/$CTNG_PRESET/bin/$CTNG_PRESET-ld
 export AR=$HOME/x-tools/$CTNG_PRESET/bin/$CTNG_PRESET-ar       
 
-# Check if "$HOME/openssl" directory exists from cache
-if [ ! -d "$HOME/x-tools" ] || [ ! -f "$PRESET_HASH_FILE" ] || [ "$(cat $PRESET_HASH_FILE)" != "$CURRENT_PRESET_HASH" ]; then
-  wget https://www.openssl.org/source/openssl-1.1.1l.tar.gz
-  tar xzf openssl-1.1.1l.tar.gz
-  cd openssl-1.1.1l
-  # Configure OpenSSL for static linking
-  ./Configure no-shared --static linux-x86_64 -fPIC --prefix=$HOME/openssl
-  make depend
-  make
-  make install
-  # Check if OpenSSL was installed successfully
-  ls -la $HOME/openssl
-
-  # Store the current hash of preset.sh after successful build
-  echo "$CURRENT_PRESET_HASH" > "$PRESET_HASH_FILE"
-fi
-
 # Set environment variables for static linking
-export OPENSSL_DIR=$HOME/openssl
 export OPENSSL_STATIC=true
-export RUSTFLAGS="-C link-arg=-static -C link-arg=-L$OPENSSL_DIR/lib"
+export RUSTFLAGS="-C link-arg=-static"
+
 # We specify the compiler that will invoke linker
 export CARGO_TARGET_X86_64_UNKNOWN_LINUX_MUSL_LINKER=$CC
+
 # Add target
 rustup target add x86_64-unknown-linux-musl
+
 # Install missing dependencies
 cargo fetch --target x86_64-unknown-linux-musl
+
 # Patch missing include in librocksdb-sys-0.16.0+8.10.0. Credit: @supertypo
 FILE_PATH=$(find $HOME/.cargo/registry/src/ -path "*/librocksdb-sys-0.16.0+8.10.0/*/offpeak_time_info.h")
+
 if [ -n "$FILE_PATH" ]; then
   sed -i '1i #include <cstdint>' "$FILE_PATH"
 else
