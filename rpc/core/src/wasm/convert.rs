e6fb2283baa043d897519aa32e4053b224937705
aspect <anton.yemelyanov@gmail.com>
2024-09-09 15:15:40 +0300

    WIP: Updates for 15-rc3 (#546)
    * update unit tests to unwrap mass to u64::max
    
    * update WASM mass APIs to be more clear with the data they produce
    
    * add fingerprint() fn to PublicKey
    
    * cleanup
    
    * fix recursive mutex in From<&Transaction> for RpcTransaction
    
    * update rust-version to 1.81.0
    
    * generator: fix incorrect change output value info triggering a sanity-check assert!

--- rpc/core/src/wasm/convert.rs
+++ rpc/core/src/wasm/convert.rs
@@ -67,7 +67,7 @@ cfg_if::cfg_if! {
                     subnetwork_id: inner.subnetwork_id.clone(),
                     gas: inner.gas,
                     payload: inner.payload.clone(),
-                    mass: tx.get_mass(),
+                    mass: inner.mass,
                     verbose_data: None,
                 }
             }
