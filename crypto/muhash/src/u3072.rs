b5793ce819e710752af15da1c7b8ebbed2cc68e7
Michael Sutton <msutton@cs.huji.ac.il>
2024-10-13 14:05:45 +0000

    comment

--- crypto/muhash/src/u3072.rs
+++ crypto/muhash/src/u3072.rs
@@ -88,6 +88,12 @@ impl U3072 {
     }
 
     fn mul(&mut self, other: &U3072) {
+        /*
+           Optimization: short-circuit when LHS is one
+               - This case is especially frequent during parallel reduce operation where the identity (one) is used for each sub-computation (at the LHS)
+               - If self ≠ one, the comparison should exit early, otherwise if they are equal -- we gain much more than we lose
+               - Benchmarks show that general performance remains the same while parallel reduction gains ~35%
+        */
         if *self == Self::one() {
             *self = *other;
             return;
