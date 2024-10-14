c63fc02117a84973b0ddf0cad05905f9d759dd26
coderofstuff <114628839+coderofstuff@users.noreply.github.com>
2024-10-13 23:27:21 -0600

    Refactoring and safety
    - Refactor to multiple functions
    - Avoid unwrap as much as possible

--- consensus/src/pipeline/virtual_processor/processor.rs
+++ consensus/src/pipeline/virtual_processor/processor.rs
@@ -47,7 +47,7 @@ use crate::{
     },
 };
 use kaspa_consensus_core::{
-    acceptance_data::AcceptanceData,
+    acceptance_data::{AcceptanceData, MergesetBlockAcceptanceData},
     api::args::{TransactionValidationArgs, TransactionValidationBatchArgs},
     block::{BlockTemplate, MutableBlock, TemplateBuildMode, TemplateTransactionSelector},
     blockstatus::BlockStatus::{StatusDisqualifiedFromChain, StatusUTXOValid},
@@ -71,7 +71,7 @@ use kaspa_consensus_notify::{
     },
     root::ConsensusNotificationRoot,
 };
-use kaspa_consensusmanager::SessionLock;
+use kaspa_consensusmanager::{SessionLock, SessionReadGuard};
 use kaspa_core::{debug, info, time::unix_now, trace, warn};
 use kaspa_database::prelude::{StoreError, StoreResultEmptyTuple, StoreResultExtensions};
 use kaspa_hashes::Hash;
@@ -1135,85 +1135,54 @@ impl VirtualStateProcessor {
     }
 
     pub fn get_utxo_return_address(&self, txid: Hash, target_daa_score: u64, source_hash: Hash, config: &Config) -> ReturnAddress {
-        // We need consistency between the past pruning points, selected chain and header store reads
-        let _guard = self.pruning_lock.blocking_read();
+        // We need consistency between the utxo_diffs_store, block_transactions_store, selected chain and header store reads
+        let guard = self.pruning_lock.blocking_read();
 
-        let sc_read = self.selected_chain_store.read();
-
-        // Pruning Point hash is always expected to be in get_compact_header_data so unwrap should never fail
-        if target_daa_score < self.headers_store.get_compact_header_data(source_hash).unwrap().daa_score {
+        let source_daa_score = if let Ok(compact_header) = self.headers_store.get_compact_header_data(source_hash) {
+            compact_header.daa_score
+        } else {
+            trace!("Did not find compact header for source hash {}", source_hash);
+            return ReturnAddress::NotFound(format!("Did not find compact header for source hash {}", source_hash));
+        };
+        if target_daa_score < source_daa_score {
             // Early exit if target daa score is lower than that of pruning point's daa score:
             return ReturnAddress::AlreadyPruned;
         }
 
-        let source_index = sc_read.get_by_hash(source_hash).unwrap();
-        let (tip_index, tip_hash) = sc_read.get_tip().unwrap();
-        let tip_daa_score = self.headers_store.get_compact_header_data(tip_hash).unwrap().daa_score;
-
-        let mut low_index = tip_index.saturating_sub(tip_daa_score.saturating_sub(target_daa_score)).max(source_index);
-        let mut high_index = tip_index;
-
-        let matching_chain_block_hash = loop {
-            // Binary search for the chain block that matches the target_daa_score
-            // 0. Get the mid point index
-            let mid = low_index + (high_index - low_index) / 2;
-
-            // 1. Get the chain block hash at that index. Error if we don't find a hash at an index
-            let hash = match sc_read.get_by_index(mid) {
+        let (matching_chain_block_hash, acceptance_data) =
+            match self.find_accepting_chain_block_hash_at_daa_score(target_daa_score, source_hash, guard) {
                 Ok(hash) => hash,
-                Err(_) => {
-                    trace!("Did not find a hash at index {}", mid);
-                    return ReturnAddress::NotFound(format!("Did not find a hash at index {}", mid));
+                Err(return_address) => {
+                    return return_address;
                 }
             };
 
-            // 2. Get the compact header so we have access to the daa_score. Error if we
-            let compact_header = match self.headers_store.get_compact_header_data(hash) {
-                Ok(compact_header) => compact_header,
-                Err(_) => {
-                    trace!("Did not find a compact header with hash {}", hash);
-                    return ReturnAddress::NotFound(format!("Did not find a compact header with hash {}", hash));
+        let (index, containing_acceptance) =
+            match self.find_tx_acceptance_data_and_index_from_block_acceptance(txid, acceptance_data.clone()) {
+                Some((index, containing_acceptance)) => (index, containing_acceptance),
+                None => {
+                    return ReturnAddress::NotFound("Did not find containing_acceptance".to_string());
                 }
             };
-
-            // 3. Compare block daa score to our target
-            match compact_header.daa_score.cmp(&target_daa_score) {
-                cmp::Ordering::Equal => {
-                    // We found the chain block we need
-                    break hash;
-                }
-                cmp::Ordering::Greater => {
-                    high_index = mid - 1;
-                }
-                cmp::Ordering::Less => {
-                    low_index = mid + 1;
-                }
-            }
-
-            if low_index > high_index {
-                return ReturnAddress::NoTxAtScore;
-            }
-        };
-
-        let acceptance_data = match self.acceptance_data_store.get(matching_chain_block_hash) {
-            Ok(acceptance_data) => acceptance_data,
-            Err(_) => {
-                return ReturnAddress::NotFound("Did not find acceptance data".to_string());
-            }
-        };
-        let (index, containing_acceptance) = match acceptance_data.iter().find_map(|mbad| {
-            let tx_arr_index =
-                mbad.accepted_transactions.iter().find_map(|tx| (tx.transaction_id == txid).then_some(tx.index_within_block as usize));
-            tx_arr_index.map(|index| (index, mbad.clone()))
-        }) {
-            Some((index, containing_acceptance)) => (index, containing_acceptance),
-            None => {
-                return ReturnAddress::NotFound("Did not find containing_acceptance".to_string());
-            }
-        };
 
         // Found Merged block containing the TXID
-        let tx = &self.block_transactions_store.get(containing_acceptance.block_hash).unwrap()[index];
+        let tx = if let Ok(block_txs) = self.block_transactions_store.get(containing_acceptance.block_hash) {
+            if let Some(tx) = block_txs.get(index) {
+                tx.clone()
+            } else {
+                warn!("Did not find tx {} at index {} in chain block tx store of {}", txid, index, containing_acceptance.block_hash);
+                return ReturnAddress::NotFound(format!(
+                    "Did not find tx {} at index {} in chain block tx store of {}",
+                    txid, index, containing_acceptance.block_hash
+                ));
+            }
+        } else {
+            warn!("Did not find tx {} at index {} in chain block tx store of {}", txid, index, containing_acceptance.block_hash);
+            return ReturnAddress::NotFound(format!(
+                "Did not find tx {} at index {} in chain block tx store of {}",
+                txid, index, containing_acceptance.block_hash
+            ));
+        };
 
         if tx.id() != txid {
             // Should never happen, but do a sanity check. This would mean something went wrong with storing block transactions
@@ -1233,7 +1202,11 @@ impl VirtualStateProcessor {
 
         let first_input_prev_outpoint = &tx.inputs[0].previous_outpoint;
         // Expected to never fail, since we found the acceptance data and therefore there must be matching diff
-        let utxo_diff = self.utxo_diffs_store.get(matching_chain_block_hash).unwrap();
+        let utxo_diff = if let Ok(res) = self.utxo_diffs_store.get(matching_chain_block_hash) {
+            res
+        } else {
+            return ReturnAddress::NotFound(format!("Did not find a utxo diff for chain block {}", matching_chain_block_hash));
+        };
         let removed_diffs = utxo_diff.removed();
 
         let spk = if let Some(utxo_entry) = removed_diffs.get(first_input_prev_outpoint) {
@@ -1246,17 +1219,37 @@ impl VirtualStateProcessor {
             // In this case, removed_diff wouldn't contain the outpoint of the created-and-immediately-spent UTXO
             // so we use the transaction (which also has acceptance data in this block) and look at its outputs
             let other_txid = first_input_prev_outpoint.transaction_id;
-            let (other_index, other_containing_acceptance) = acceptance_data
-                .iter()
-                .find_map(|ombad| {
-                    let otx_arr_index = ombad
-                        .accepted_transactions
-                        .iter()
-                        .find_map(|otx| (otx.transaction_id == other_txid).then_some(otx.index_within_block as usize));
-                    otx_arr_index.map(|index| (index, ombad.clone()))
-                })
-                .expect("The other transaction's acceptance data must also be in the same block in this case");
-            let other_tx = &self.block_transactions_store.get(other_containing_acceptance.block_hash).unwrap()[other_index];
+            let (other_index, other_containing_acceptance) =
+                if let Some(res) = self.find_tx_acceptance_data_and_index_from_block_acceptance(other_txid, acceptance_data) {
+                    res
+                } else {
+                    return ReturnAddress::NotFound(
+                        "The other transaction's acceptance data must also be in the same block in this case".to_string(),
+                    );
+                };
+            let other_tx = if let Ok(block_txs) = self.block_transactions_store.get(other_containing_acceptance.block_hash) {
+                if let Some(tx) = block_txs.get(other_index) {
+                    tx.clone()
+                } else {
+                    warn!(
+                        "Did not find tx {} at index {} in chain block tx store of {}",
+                        other_txid, other_index, other_containing_acceptance.block_hash
+                    );
+                    return ReturnAddress::NotFound(format!(
+                        "Did not find tx {} at index {} in chain block tx store of {}",
+                        other_txid, other_index, other_containing_acceptance.block_hash
+                    ));
+                }
+            } else {
+                warn!(
+                    "Did not find tx {} at index {} in chain block tx store of {}",
+                    other_txid, other_index, other_containing_acceptance.block_hash
+                );
+                return ReturnAddress::NotFound(format!(
+                    "Did not find tx {} at index {} in chain block tx store of {}",
+                    other_txid, other_index, other_containing_acceptance.block_hash
+                ));
+            };
 
             other_tx.outputs[first_input_prev_outpoint.index as usize].script_public_key.clone()
         };
@@ -1266,6 +1259,84 @@ impl VirtualStateProcessor {
         } else {
             ReturnAddress::NonStandard
         }
+    }
+
+    fn find_accepting_chain_block_hash_at_daa_score(
+        &self,
+        target_daa_score: u64,
+        source_hash: Hash,
+        _guard: SessionReadGuard,
+    ) -> Result<(Hash, Arc<Vec<MergesetBlockAcceptanceData>>), ReturnAddress> {
+        let sc_read = self.selected_chain_store.read();
+
+        let source_index = sc_read
+            .get_by_hash(source_hash)
+            .map_err(|_| ReturnAddress::NotFound(format!("Did not find index for hash {}", source_hash)))?;
+        let (tip_index, tip_hash) = sc_read.get_tip().map_err(|_| ReturnAddress::NotFound("Did not find tip data".to_string()))?;
+        let tip_daa_score = self
+            .headers_store
+            .get_compact_header_data(tip_hash)
+            .map(|tip| tip.daa_score)
+            .map_err(|_| ReturnAddress::NotFound(format!("Did not find compact header data for tip hash {}", tip_hash)))?;
+
+        let mut low_index = tip_index.saturating_sub(tip_daa_score.saturating_sub(target_daa_score)).max(source_index);
+        let mut high_index = tip_index;
+
+        let matching_chain_block_hash = loop {
+            // Binary search for the chain block that matches the target_daa_score
+            // 0. Get the mid point index
+            let mid = low_index + (high_index - low_index) / 2;
+
+            // 1. Get the chain block hash at that index. Error if we don't find a hash at an index
+            let hash = sc_read.get_by_index(mid).map_err(|_| {
+                trace!("Did not find a hash at index {}", mid);
+                ReturnAddress::NotFound(format!("Did not find a hash at index {}", mid))
+            })?;
+
+            // 2. Get the compact header so we have access to the daa_score. Error if we
+            let compact_header = self.headers_store.get_compact_header_data(hash).map_err(|_| {
+                trace!("Did not find a compact header with hash {}", hash);
+                ReturnAddress::NotFound(format!("Did not find a compact header with hash {}", hash))
+            })?;
+
+            // 3. Compare block daa score to our target
+            match compact_header.daa_score.cmp(&target_daa_score) {
+                cmp::Ordering::Equal => {
+                    // We found the chain block we need
+                    break hash;
+                }
+                cmp::Ordering::Greater => {
+                    high_index = mid - 1;
+                }
+                cmp::Ordering::Less => {
+                    low_index = mid + 1;
+                }
+            }
+
+            if low_index > high_index {
+                return Err(ReturnAddress::NoTxAtScore);
+            }
+        };
+
+        let acceptance_data = self.acceptance_data_store.get(matching_chain_block_hash).map_err(|_| {
+            ReturnAddress::NotFound(format!("Did not find acceptance data for chain block {}", matching_chain_block_hash))
+        })?;
+
+        Ok((matching_chain_block_hash, acceptance_data))
+    }
+
+    fn find_tx_acceptance_data_and_index_from_block_acceptance(
+        &self,
+        tx_id: Hash,
+        block_acceptance_data: Arc<Vec<MergesetBlockAcceptanceData>>,
+    ) -> Option<(usize, MergesetBlockAcceptanceData)> {
+        block_acceptance_data.iter().find_map(|mbad| {
+            let tx_arr_index = mbad
+                .accepted_transactions
+                .iter()
+                .find_map(|tx| (tx.transaction_id == tx_id).then_some(tx.index_within_block as usize));
+            tx_arr_index.map(|index| (index, mbad.clone()))
+        })
     }
 
     pub fn are_pruning_points_violating_finality(&self, pp_list: PruningPointsList) -> bool {
