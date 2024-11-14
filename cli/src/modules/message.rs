c2aeba32bea3e38c359e6db44443dc0b3a3c709e
witter-deland <87846830+witter-deland@users.noreply.github.com>
2024-11-14 15:36:21 +0700

    feat: add signMessage noAuxRand option for kaspa wasm (#587)
    * feat: add signMessageWithoutRand method for kaspa wasm
    
    * enhance: sign message api
    
    * fix: unit test fail
    
    * chore: update noAuxRand of ISignMessage
    
    * chore: add sign message demo for noAuxRand

--- cli/src/modules/message.rs
+++ cli/src/modules/message.rs
@@ -1,5 +1,6 @@
 use kaspa_addresses::Version;
 use kaspa_bip32::secp256k1::XOnlyPublicKey;
+use kaspa_wallet_core::message::SignMessageOptions;
 use kaspa_wallet_core::{
     account::{BIP32_ACCOUNT_KIND, KEYPAIR_ACCOUNT_KIND},
     message::{sign_message, verify_message, PersonalMessage},
@@ -87,8 +88,9 @@ impl Message {
 
         let pm = PersonalMessage(message);
         let privkey = self.get_address_private_key(&ctx, kaspa_address).await?;
+        let sign_options = SignMessageOptions { no_aux_rand: false };
 
-        let sig_result = sign_message(&pm, &privkey);
+        let sig_result = sign_message(&pm, &privkey, &sign_options);
 
         match sig_result {
             Ok(signature) => {
