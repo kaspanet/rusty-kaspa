458fe0295c9e6e169b12b6fe11db00985adf4929
coderofstuff <114628839+coderofstuff@users.noreply.github.com>
2025-02-11 22:45:34 -0700

    Implement Sync Rate Rule

--- protocol/mining/src/rule_engine.rs
+++ protocol/mining/src/rule_engine.rs
@@ -1,14 +1,11 @@
 use std::{
-    sync::{
-        atomic::{AtomicBool, Ordering},
-        Arc,
-    },
+    sync::{atomic::AtomicBool, Arc},
     time::{Duration, Instant},
 };
 
 use kaspa_consensus_core::{
     api::counters::ProcessingCounters,
-    config::{params::NEW_DIFFICULTY_WINDOW_DURATION, Config},
+    config::Config,
     daa_score_timestamp::DaaScoreTimestamp,
     mining_rules::MiningRules,
     network::NetworkType::{Mainnet, Testnet},
@@ -20,14 +17,14 @@ use kaspa_core::{
         tick::{TickReason, TickService},
     },
     time::unix_now,
-    trace, warn,
+    trace,
 };
 use kaspa_p2p_lib::Hub;
 
-use crate::rules::mining_rule::MiningRule;
+use crate::rules::{mining_rule::MiningRule, sync_rate_rule::SyncRateRule, ExtraData};
 
 const RULE_ENGINE: &str = "mining-rule-engine";
-const SYNC_RATE_THRESHOLD: f64 = 0.10;
+pub const SNAPSHOT_INTERVAL: u64 = 10;
 
 #[derive(Clone)]
 pub struct MiningRuleEngine {
@@ -44,13 +41,11 @@ pub struct MiningRuleEngine {
 
 impl MiningRuleEngine {
     pub async fn worker(self: &Arc<MiningRuleEngine>) {
-        println!(module_path!());
-        let snapshot_interval = 10;
         let mut last_snapshot = self.processing_counters.snapshot();
         let mut last_log_time = Instant::now();
         loop {
             // START: Sync monitor
-            if let TickReason::Shutdown = self.tick_service.tick(Duration::from_secs(snapshot_interval)).await {
+            if let TickReason::Shutdown = self.tick_service.tick(Duration::from_secs(SNAPSHOT_INTERVAL)).await {
                 // Let the system print final logs before exiting
                 tokio::time::sleep(Duration::from_millis(500)).await;
                 break;
@@ -70,60 +65,24 @@ impl MiningRuleEngine {
             if elapsed_time.as_secs() > 0 {
                 let session = self.consensus_manager.consensus().unguarded_session();
                 let sink_daa_timestamp = session.async_get_sink_daa_score_timestamp().await;
-                let expected_blocks =
-                    (elapsed_time.as_millis() as u64) / self.config.target_time_per_block().get(sink_daa_timestamp.daa_score);
-                let received_blocks = delta.body_counts.max(delta.header_counts);
-                let rate: f64 = (received_blocks as f64) / (expected_blocks as f64);
 
                 let finality_point = session.async_finality_point().await;
                 let finality_point_timestamp = session.async_get_header(finality_point).await.unwrap().timestamp;
-                // Finality point is considered "recent" if it is within 3 finality durations from the current time
-                let is_finality_recent = finality_point_timestamp
-                    >= unix_now()
-                        .saturating_sub(self.config.finality_duration_in_milliseconds().get(sink_daa_timestamp.daa_score) * 3);
 
-                trace!(
-                    "Sync rate: {:.2} | Finality point recent: {} | Elapsed time: {}s | Connected: {} | Found/Expected blocks: {}/{}",
-                    rate,
-                    is_finality_recent,
-                    elapsed_time.as_secs(),
-                    delta.body_counts,
-                    self.has_sufficient_peer_connectivity(),
-                    expected_blocks,
-                );
+                let extra_data = ExtraData {
+                    finality_point_timestamp,
+                    target_time_per_block: self.config.target_time_per_block().get(sink_daa_timestamp.daa_score),
+                    has_sufficient_peer_connectivity: self.has_sufficient_peer_connectivity(),
+                    finality_duration: self.config.finality_duration_in_milliseconds().get(sink_daa_timestamp.daa_score),
+                    elapsed_time,
+                };
 
-                if is_finality_recent && rate < SYNC_RATE_THRESHOLD {
-                    // if sync rate rule conditions are met:
-                    if let Ok(false) = self.use_sync_rate_rule.compare_exchange(false, true, Ordering::Relaxed, Ordering::Relaxed) {
-                        warn!("Sync rate {:.2} is below threshold: {}", rate, SYNC_RATE_THRESHOLD);
-                    }
-                } else {
-                    // else when sync rate conditions are not met:
-                    if let Ok(true) = self.use_sync_rate_rule.compare_exchange(true, false, Ordering::Relaxed, Ordering::Relaxed) {
-                        if !is_finality_recent {
-                            warn!("Sync rate {:.2} recovered: {} by entering IBD", rate, SYNC_RATE_THRESHOLD);
-                        } else {
-                            warn!("Sync rate {:.2} recovered: {}", rate, SYNC_RATE_THRESHOLD);
-                        }
-                    } else if !is_finality_recent {
-                        trace!("Finality period is old. Timestamp: {}. Sync rate: {:.2}", finality_point_timestamp, rate);
-                    }
-                }
-
-                // END - Sync monitor
-
-                // START - Rule Engine
                 trace!("Current Mining Rule: {:?}", self.mining_rules);
 
-                // Blue Parents Only Check:
+                // Check for all the rules
                 for rule in &self.rules {
-                    rule.check_rule(&snapshot);
+                    rule.check_rule(&delta, &extra_data);
                 }
-
-                // No Transactions Check:
-                // TODO: implement this part
-
-                // End - Rule Engine
             }
 
             last_snapshot = snapshot;
@@ -139,18 +98,10 @@ impl MiningRuleEngine {
         hub: Hub,
         mining_rules: Arc<MiningRules>,
     ) -> Self {
-        let rules: Vec<Arc<(dyn MiningRule + 'static)>> = vec![];
+        let use_sync_rate_rule = Arc::new(AtomicBool::new(false));
+        let rules: Vec<Arc<(dyn MiningRule + 'static)>> = vec![Arc::new(SyncRateRule::new(use_sync_rate_rule.clone()))];
 
-        Self {
-            consensus_manager,
-            config,
-            processing_counters,
-            tick_service,
-            hub,
-            use_sync_rate_rule: Arc::new(AtomicBool::new(false)),
-            mining_rules,
-            rules,
-        }
+        Self { consensus_manager, config, processing_counters, tick_service, hub, use_sync_rate_rule, mining_rules, rules }
     }
 
     pub fn should_mine(&self, sink_daa_score_timestamp: DaaScoreTimestamp) -> bool {
@@ -169,27 +120,16 @@ impl MiningRuleEngine {
     pub fn is_nearly_synced(&self, sink_daa_score_timestamp: DaaScoreTimestamp) -> bool {
         let sink_timestamp = sink_daa_score_timestamp.timestamp;
 
-        if self.config.net.is_mainnet() {
-            // We consider the node close to being synced if the sink (virtual selected parent) block
-            // timestamp is within DAA window duration far in the past. Blocks mined over such DAG state would
-            // enter the DAA window of fully-synced nodes and thus contribute to overall network difficulty
-            //
-            // [Crescendo]: both durations are nearly equal so this decision is negligible
-            unix_now()
-                < sink_timestamp
-                    + self.config.expected_difficulty_window_duration_in_milliseconds().get(sink_daa_score_timestamp.daa_score)
-        } else {
-            // For testnets we consider the node to be synced if the sink timestamp is within a time range which
-            // is overwhelmingly unlikely to pass without mined blocks even if net hashrate decreased dramatically.
-            //
-            // This period is smaller than the above mainnet calculation in order to ensure that an IBDing miner
-            // with significant testnet hashrate does not overwhelm the network with deep side-DAGs.
-            //
-            // We use DAA duration as baseline and scale it down with BPS (and divide by 3 for mining only when very close to current time on 10BPS testnets)
-            let max_expected_duration_without_blocks_in_milliseconds =
-                self.config.prior_target_time_per_block * NEW_DIFFICULTY_WINDOW_DURATION / 3; // = DAA duration in milliseconds / bps / 3
-            unix_now() < sink_timestamp + max_expected_duration_without_blocks_in_milliseconds
-        }
+        // We consider the node close to being synced if the sink (virtual selected parent) block
+        // timestamp is within a quarter of the DAA window duration far in the past. Blocks mined over such DAG state would
+        // enter the DAA window of fully-synced nodes and thus contribute to overall network difficulty
+        //
+        // [Crescendo]: both durations are nearly equal so this decision is negligible
+        let synced_threshold =
+            self.config.expected_difficulty_window_duration_in_milliseconds().get(sink_daa_score_timestamp.daa_score) / 4;
+
+        // Roughly 10mins in all networks
+        unix_now() < sink_timestamp + synced_threshold
     }
 
     fn has_sufficient_peer_connectivity(&self) -> bool {
