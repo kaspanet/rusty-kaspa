e1cf33e62314bae09993a7aa7f2e5eb34ea952fe
supr <98590479+miningexperiments@users.noreply.github.com>
2025-02-13 11:41:55 +0200

    Update validate.rs

--- consensus/src/processes/pruning_proof/validate.rs
+++ consensus/src/processes/pruning_proof/validate.rs
@@ -175,7 +175,7 @@ impl PruningProofManager {
 
         let headers_estimate = self.estimate_proof_unique_size(proof);
 
-        let (db_lifetime, db) = kaspa_database::create_temp_db!(ConnBuilder::default().with_files_limit(10));
+        let (db_lifetime, db) = kaspa_database::create_temp_db!(ConnBuilder::default().with_files_limit(10)).expect("Failed to create temp db");
         let cache_policy = CachePolicy::Count(2 * self.pruning_proof_m as usize);
         let headers_store =
             Arc::new(DbHeadersStore::new(db.clone(), CachePolicy::Count(headers_estimate), CachePolicy::Count(headers_estimate)));
