88080d3b9732389246ebf86d51b381e5d6f05e19
coderofstuff <114628839+coderofstuff@users.noreply.github.com>
2025-02-13 23:33:40 -0700

    Use virtual resolve count to calc processing average

--- protocol/mining/src/rules/blue_parents_only_rule.rs
+++ protocol/mining/src/rules/blue_parents_only_rule.rs
@@ -30,41 +30,36 @@ impl BlueParentsOnlyRule {
 
 impl MiningRule for BlueParentsOnlyRule {
     fn check_rule(&self, delta: &ProcessingCountersSnapshot, extra_data: &ExtraData) {
-        let received_blocks = delta.body_counts.max(delta.header_counts) as f64;
         let sink_daa_score = extra_data.sink_daa_score_timestamp.daa_score;
         // DAA score may not be monotonic, so use saturating_sub
         let score_since_trigger = sink_daa_score.saturating_sub(self.trigger_daa_score.load(Ordering::Relaxed));
-        let virtual_processing_avg_time_per_block_ms =
-            if received_blocks > 0.0 { (delta.virtual_processing_time as f64) / received_blocks } else { 0.0 };
+        let virtual_average_processing_time = if delta.virtual_resolve_counts > 0 {
+            (delta.virtual_processing_time as f64) / delta.virtual_resolve_counts as f64
+        } else {
+            0.0
+        };
 
-        if self.is_enabled.load(Ordering::Relaxed) {
+        if self.is_enabled.load(Ordering::SeqCst) {
             // Rule is triggered. Check for recovery
-            if virtual_processing_avg_time_per_block_ms < VIRTUAL_PROCESSING_RECOVERY_THRESHOLD
-                && score_since_trigger >= extra_data.merge_depth
+            if virtual_average_processing_time < VIRTUAL_PROCESSING_RECOVERY_THRESHOLD && score_since_trigger >= extra_data.merge_depth
             {
                 self.is_enabled.store(false, Ordering::SeqCst);
-                warn!(
-                    "BlueParentsOnlyRule: recovered | Avg virtual processing time: {:.2}ms",
-                    virtual_processing_avg_time_per_block_ms,
-                );
+                warn!("BlueParentsOnlyRule: recovered | Avg virtual processing time: {:.2}ms", virtual_average_processing_time);
             } else {
                 trace!(
                     "BlueParentsOnlyRule: active | Avg virtual processing time: {:.2}ms | Score since trigger: {}",
-                    virtual_processing_avg_time_per_block_ms,
+                    virtual_average_processing_time,
                     score_since_trigger
                 );
             }
         } else {
             // Rule is not triggered. Check for trigger
-            if virtual_processing_avg_time_per_block_ms > VIRTUAL_PROCESSING_TRIGGER_THRESHOLD {
+            if virtual_average_processing_time > VIRTUAL_PROCESSING_TRIGGER_THRESHOLD {
                 self.is_enabled.store(true, Ordering::SeqCst);
-                self.trigger_daa_score.store(sink_daa_score, Ordering::Relaxed);
-                warn!(
-                    "BlueParentsOnlyRule: triggered | Avg virtual processing time: {:.2}ms",
-                    virtual_processing_avg_time_per_block_ms
-                );
+                self.trigger_daa_score.store(sink_daa_score, Ordering::SeqCst);
+                warn!("BlueParentsOnlyRule: triggered | Avg virtual processing time: {:.2}ms", virtual_average_processing_time);
             } else {
-                trace!("BlueParentsOnlyRule: normal | Avg virtual processing time: {:.2}ms", virtual_processing_avg_time_per_block_ms);
+                trace!("BlueParentsOnlyRule: normal | Avg virtual processing time: {:.2}ms", virtual_average_processing_time);
             }
         }
     }
