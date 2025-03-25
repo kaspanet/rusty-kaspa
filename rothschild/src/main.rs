fe425b66c317cf610a3cf11a5a188a77c1b7fc9a
Michael Sutton <msutton@cs.huji.ac.il>
2025-03-25 13:59:44 +0000

    rothschild: reduce compounding phase to 8 inputs for simulating more reasonable average behavior

--- rothschild/src/main.rs
+++ rothschild/src/main.rs
@@ -555,7 +555,7 @@ fn select_utxos(
     next_available_utxo_index: &mut usize,
     tx_config: &TxConfig,
 ) -> (Vec<(TransactionOutpoint, UtxoEntry)>, u64) {
-    const MAX_UTXOS: usize = 84;
+    const MAX_UTXOS: usize = 8;
     let mut selected_amount: u64 = 0;
     let mut selected = Vec::new();
     let mut rng = thread_rng();
