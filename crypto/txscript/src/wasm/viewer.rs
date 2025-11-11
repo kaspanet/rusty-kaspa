4f64433375abc466c0fbb0f35a3b0b9a20aac600
IzioDev <9900846+IzioDev@users.noreply.github.com>
2025-11-11 18:15:02 +0100

    wasm

--- crypto/txscript/src/wasm/viewer.rs
+++ crypto/txscript/src/wasm/viewer.rs
@@ -1,18 +1,18 @@
 use wasm_bindgen::prelude::*;
-use workflow_wasm::prelude::ObjectExtension;
+use workflow_wasm::prelude::{Cast, ObjectExtension, TryCastFromJs};
 
 use crate::{error::Error, result::Result, viewer::ScriptViewerOptions};
 
 #[wasm_bindgen(typescript_custom_section)]
 const TS_SCRIPT_VIEWER_OPTIONS: &'static str = r#"
 /**
- * Interface defining the structure of a transaction.
+ * Interface defining the structure of a script viewer option.
  *
  * @category txscript
  */
 export interface IScriptViewerOptions {
     /** Wether or not to try disassemble sub-script (redeem script) */
-    contains_redeem_script?: bool;
+    contains_redeem_script?: boolean;
 }
 "#;
 
@@ -30,5 +30,18 @@ impl TryFrom<IScriptViewerOptions> for ScriptViewerOptions {
         let contains_redeem_script = object.get_bool("contains_redeem_script").ok().unwrap_or(false);
 
         Ok(ScriptViewerOptions { contains_redeem_script })
+    }
+}
+
+impl TryCastFromJs for ScriptViewerOptions {
+    type Error = Error;
+    fn try_cast_from<'a, R>(value: &'a R) -> std::result::Result<Cast<'a, Self>, Self::Error>
+    where
+        R: AsRef<JsValue> + 'a,
+    {
+        Self::resolve_cast(value, || {
+            // todo: handle options
+            Ok(ScriptViewerOptions { ..Default::default() }.into())
+        })
     }
 }
