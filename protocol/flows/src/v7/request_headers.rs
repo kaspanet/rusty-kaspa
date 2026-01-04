73c6d5d2c670a0c5789cc7b755fdbd68ff470d8f
Manyfestation <240733973+Manyfestation@users.noreply.github.com>
2026-01-04 15:05:11 +0200

    P2p compressed header (#777)
    * compressed P2P headers with backward compatibility
    
    * remove redundant length check
    
    * rename for improved clarity
    
    * wrap header proto types with new `Versioned` struct
    
    * rename `ParentsByLevel::inner` to `raw`
    
    * refactor determine_header_format to `From<32>' treat
    
    * Add validation for fully compressed parents
    
    * move header format calculation outside flow loop
    
    * pass header format to flows explicity

--- protocol/flows/src/v7/request_headers.rs
+++ protocol/flows/src/v7/request_headers.rs
@@ -4,6 +4,7 @@ use kaspa_consensus_core::api::ConsensusApi;
 use kaspa_hashes::Hash;
 use kaspa_p2p_lib::{
     common::ProtocolError,
+    convert::header::HeaderFormat,
     dequeue, dequeue_with_request_id, make_response,
     pb::{self, kaspad_message::Payload, BlockHeadersMessage, DoneHeadersMessage},
     IncomingRoute, Router,
@@ -16,6 +17,7 @@ pub struct RequestHeadersFlow {
     ctx: FlowContext,
     router: Arc<Router>,
     incoming_route: IncomingRoute,
+    header_format: HeaderFormat,
 }
 
 #[async_trait::async_trait]
@@ -30,14 +32,15 @@ impl Flow for RequestHeadersFlow {
 }
 
 impl RequestHeadersFlow {
-    pub fn new(ctx: FlowContext, router: Arc<Router>, incoming_route: IncomingRoute) -> Self {
-        Self { ctx, router, incoming_route }
+    pub fn new(ctx: FlowContext, router: Arc<Router>, incoming_route: IncomingRoute, header_format: HeaderFormat) -> Self {
+        Self { ctx, router, incoming_route, header_format }
     }
 
     async fn start_impl(&mut self) -> Result<(), ProtocolError> {
         const MAX_BLOCKS: usize = 1 << 10;
         // Internal consensus logic requires that `max_blocks > mergeset_size_limit`
         let max_blocks = max(MAX_BLOCKS, self.ctx.config.mergeset_size_limit() as usize + 1);
+        let header_format = self.header_format;
 
         loop {
             let (msg, request_id) = dequeue_with_request_id!(self.incoming_route, Payload::RequestHeaders)?;
@@ -65,7 +68,7 @@ impl RequestHeadersFlow {
 
                 // We spawn the I/O-intensive operation of reading a bunch of headers as a tokio blocking task
                 let (block_headers, last) =
-                    session.spawn_blocking(move |c| Self::get_headers_between(c, low, high, max_blocks)).await?;
+                    session.spawn_blocking(move |c| Self::get_headers_between(c, low, high, max_blocks, header_format)).await?;
                 debug!("Got {} header hashes above {}", block_headers.len(), low);
                 low = last;
                 self.router.enqueue(make_response!(Payload::BlockHeaders, BlockHeadersMessage { block_headers }, request_id)).await?;
@@ -85,13 +88,14 @@ impl RequestHeadersFlow {
         low: Hash,
         high: Hash,
         max_blocks: usize,
+        header_format: kaspa_p2p_lib::convert::header::HeaderFormat,
     ) -> Result<(Vec<pb::BlockHeader>, Hash), ProtocolError> {
         let hashes = consensus.get_hashes_between(low, high, max_blocks)?.0;
         let last = *hashes.last().expect("caller ensured that high and low are valid and different");
         debug!("obtained {} header hashes above {}", hashes.len(), low);
         let mut block_headers = Vec::with_capacity(hashes.len());
         for hash in hashes {
-            block_headers.push(<pb::BlockHeader>::from(&*consensus.get_header(hash)?));
+            block_headers.push(<pb::BlockHeader>::from((header_format, &*consensus.get_header(hash)?)));
         }
         Ok((block_headers, last))
     }
