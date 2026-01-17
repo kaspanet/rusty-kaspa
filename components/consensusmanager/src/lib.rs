0c8597d1f4cc6bad1b459c4918fa869e4ffe2a7e
freshair18 <139654058+freshair18@users.noreply.github.com>
2026-01-18 00:22:53 +0200

    Proper utxoindex resyncing post utxo download (#827)
    * reset handlers function
    
    * comments addressed
    
    * reverted bad versioning
    
    * cargo.lock
    
    * cargo.lock reverted changes

--- components/consensusmanager/src/lib.rs
+++ components/consensusmanager/src/lib.rs
@@ -171,6 +171,14 @@ impl ConsensusManager {
     pub fn delete_staging_entry(&self) {
         self.factory.delete_staging_entry();
     }
+
+    /// Explicit resetting of  service handlers
+    pub fn invoke_consensus_reset_handlers(&self) {
+        let handlers = self.inner.read().consensus_reset_handlers.iter().cloned().collect_vec();
+        for handler in handlers {
+            handler.handle_consensus_reset();
+        }
+    }
 }
 
 impl Service for ConsensusManager {
@@ -206,10 +214,7 @@ impl StagingConsensus {
         prev.ctl.stop();
         g.current.ctl.make_active();
         drop(g);
-        let handlers = self.manager.inner.read().consensus_reset_handlers.iter().cloned().collect_vec();
-        for handler in handlers {
-            handler.handle_consensus_reset();
-        }
+        self.manager.invoke_consensus_reset_handlers();
         // Drop `prev` so that deletion below succeeds
         drop(prev);
         // Staging was committed and is now the active consensus so we can delete
