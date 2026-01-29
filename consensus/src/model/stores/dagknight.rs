ae02f478d387b76c6c9b46587a1f11e67e938770
coderofstuff <114628839+coderofstuff@users.noreply.github.com>
2026-01-28 21:13:00 -0700

    [DK-pruning] Initial implementation of pruning

--- consensus/src/model/stores/dagknight.rs
+++ consensus/src/model/stores/dagknight.rs
@@ -35,8 +35,8 @@ impl DagknightKey {
     pub fn new(root_hash: Hash, pov_hash: Hash, k: KType, free_search: bool) -> Self {
         let mut bytes = [0u8; kaspa_hashes::HASH_SIZE * 2 + 2];
         bytes[..kaspa_hashes::HASH_SIZE].copy_from_slice(root_hash.as_ref());
-        bytes[kaspa_hashes::HASH_SIZE..(2 * kaspa_hashes::HASH_SIZE)].copy_from_slice(pov_hash.as_ref());
-        bytes[2 * kaspa_hashes::HASH_SIZE] = k as u8;
+        bytes[kaspa_hashes::HASH_SIZE] = k as u8;
+        bytes[(kaspa_hashes::HASH_SIZE + 1)..(2 * kaspa_hashes::HASH_SIZE + 1)].copy_from_slice(pov_hash.as_ref());
         bytes[(2 * kaspa_hashes::HASH_SIZE) + 1] = if free_search { 1 } else { 0 };
 
         Self { pov_hash, root_hash, k, free_search, bytes }
@@ -63,18 +63,23 @@ impl std::hash::Hash for DagknightKey {
         self.root_hash.hash(state);
         self.k.hash(state);
         self.pov_hash.hash(state);
+        self.free_search.hash(state);
     }
 }
 
 impl PartialEq for DagknightKey {
     fn eq(&self, other: &Self) -> bool {
-        self.pov_hash == other.pov_hash && self.root_hash == other.root_hash && self.k == other.k
+        self.pov_hash == other.pov_hash
+            && self.k == other.k
+            && self.root_hash == other.root_hash
+            && self.free_search == other.free_search
     }
 }
 
 pub trait DagknightStore {
     fn insert(&self, key: DagknightKey, dk_data: Arc<GhostdagData>) -> Result<(), StoreError>;
     fn delete(&self, key: DagknightKey) -> Result<(), StoreError>;
+    fn delete_rooted_range(&self, batch: &mut WriteBatch, hash: Hash) -> Result<u32, StoreError>;
 }
 
 impl MemoryDagknightStore {
@@ -112,6 +117,10 @@ impl DagknightStore for MemoryDagknightStore {
         self.dk_map.borrow_mut().remove(&key);
 
         Ok(())
+    }
+
+    fn delete_rooted_range(&self, _batch: &mut WriteBatch, _hash: Hash) -> Result<u32, StoreError> {
+        unimplemented!()
     }
 }
 
@@ -171,5 +180,47 @@ impl DagknightStore for DbDagknightStore {
         self.access.delete(BatchDbWriter::new(&mut batch), key)?;
         self.db.write(batch)?;
         Ok(())
+    }
+
+    fn delete_rooted_range(&self, batch: &mut WriteBatch, hash: Hash) -> Result<u32, StoreError> {
+        // delete records that have a prefix rooted at this DK store key + hash
+        let root_bytes_prefix = {
+            let mut bytes = Vec::with_capacity(kaspa_hashes::HASH_SIZE + 1);
+            bytes.extend(DatabaseStorePrefixes::DagKnight.as_ref());
+            bytes.extend_from_slice(hash.as_ref());
+            bytes
+        };
+        let start_conflict_genesis_bytes = {
+            let mut bytes = Vec::with_capacity(kaspa_hashes::HASH_SIZE + 2);
+            bytes.extend_from_slice(&root_bytes_prefix);
+            bytes.push(0); // k = 0 u16 first byte
+            bytes.push(0); // k = 0 u16 second byte
+            bytes
+        };
+        let end_conflict_genesis_bytes = {
+            let mut bytes = Vec::with_capacity(kaspa_hashes::HASH_SIZE + 2);
+            bytes.extend_from_slice(&root_bytes_prefix);
+            // TODO[DK]: This range check misses entries where k = u16::MAX. However, we don't expect k to reach that value anyway
+            // in practice so we don't expect records to exist here as well. In the DK implementation, k may be clamped to max out
+            // lower than k = u16::MAX
+            bytes.push(0xFF); // k = 0xFFFF u16 first byte
+            bytes.push(0xFF); // k = 0xFFFF u16 second byte
+            bytes
+        };
+        // TODO[DK]: count keys in range. Possibly would be removed.
+        let mut count = 0;
+        let mut iterator = self.db.raw_iterator();
+        iterator.seek(&start_conflict_genesis_bytes);
+        while iterator.valid() {
+            let key = iterator.key();
+            if key.unwrap() >= end_conflict_genesis_bytes.as_slice() {
+                break;
+            }
+            count += 1;
+            iterator.next();
+        }
+        // Perform the range delete
+        batch.delete_range(start_conflict_genesis_bytes, end_conflict_genesis_bytes);
+        Ok(count)
     }
 }
