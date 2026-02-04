24c7dd203cdb2cadf20c4da055e0a8dcdf85ae69
coderofstuff <114628839+coderofstuff@users.noreply.github.com>
2026-02-03 22:04:09 -0700

    Add Debug to SortableBlock

--- consensus/src/processes/ghostdag/ordering.rs
+++ consensus/src/processes/ghostdag/ordering.rs
@@ -11,7 +11,7 @@ use crate::model::{
 
 use super::protocol::GhostdagManager;
 
-#[derive(Eq, Clone, Serialize, Deserialize)]
+#[derive(Eq, Clone, Serialize, Deserialize, Debug)]
 pub struct SortableBlock {
     pub hash: Hash,
     pub blue_work: BlueWorkType,
