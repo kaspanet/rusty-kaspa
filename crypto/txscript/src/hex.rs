ba80815429f1c439248b6c9bdbd6dc038b62ceeb
Michael Sutton <msutton@cs.huji.ac.il>
2026-02-05 08:58:54 +0000

    remove redundant check (hex)

--- crypto/txscript/src/hex.rs
+++ crypto/txscript/src/hex.rs
@@ -2,10 +2,6 @@
 #[inline]
 pub fn decode<T: AsRef<[u8]>>(data: T) -> Result<Vec<u8>, faster_hex::Error> {
     let input = data.as_ref();
-    if input.len() % 2 != 0 {
-        return Err(faster_hex::Error::InvalidLength(input.len()));
-    }
-
     let mut bytes = vec![0u8; input.len() / 2];
     faster_hex::hex_decode(input, &mut bytes)?;
     Ok(bytes)
