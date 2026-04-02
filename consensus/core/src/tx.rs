53a9132831839414b1086c25387d63c8dccc36be
143672 <biryukov.maxim.vladimirovich@yandex.ru>
2026-04-02 16:34:34 +0400

    return seq_commit_tx_digest helper method back

--- consensus/core/src/tx.rs
+++ consensus/core/src/tx.rs
@@ -438,6 +438,10 @@ pub trait VerifiableTransaction {
         self.tx().payload_digest()
     }
 
+    fn seq_commit_digest(&self) -> Hash {
+        self.tx().seq_commit_digest()
+    }
+
     fn version(&self) -> u16 {
         self.tx().version
     }
