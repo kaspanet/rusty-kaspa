43f96576558e1e835d2cf79e8588a5c17de5fd1d
Saefstroem <139136798+saefstroem@users.noreply.github.com>
2026-05-01 12:30:04 +0800

    Add comments + clippy

--- crypto/txscript/src/zk_precompiles/risc0/zk_to_script/groth16/mod.rs
+++ crypto/txscript/src/zk_precompiles/risc0/zk_to_script/groth16/mod.rs
@@ -18,6 +18,8 @@ use ark_serialize::{CanonicalDeserialize, CanonicalSerialize};
 use risc0_binfmt::Digestible;
 use risc0_groth16::Seal;
 use risc0_zkvm::{Digest, Groth16Receipt, Groth16ReceiptVerifierParameters, sha};
+
+/// Splits a r0 digest into two 32 byte arrays
 fn split_digest_bytes(d: Digest) -> ([u8; 32], [u8; 32]) {
     let bytes = d.as_bytes();
     let mut lo = [0u8; 32];
@@ -32,37 +34,57 @@ impl R0ScriptBuilder {
     /// This script unlocks the UTXO if the verification of the receipt
     /// succeeds.
     pub fn from_groth<Claim: Digestible + Clone>(receipt: &Groth16Receipt<Claim>) -> Result<ScriptBuilder> {
+        
+        // Get the default parameters for the g16 verifier
         let params = Groth16ReceiptVerifierParameters::default();
         let seal = &receipt.seal;
         let digested_claim = receipt.claim.digest::<sha::Impl>();
+
+        // Because a 32 byte sha256 digest can be too large 
+        // for the bn254 field, we split the digest into two 16byte parts
+        // and pad them. This way we can guarantee that we wont overflow.
         let (a0, a1) = split_digest_bytes(params.control_root);
         let (c0, c1) = split_digest_bytes(digested_claim);
         let id_bn254: [u8; 32] = params.bn254_control_id.into();
+
+        // Decode the R0 seal.
         let seal = Seal::decode(seal).map_err(|e| R0Error::SealDecoding(e.to_string()))?;
+
+        // Utilize the groth16 R0 VK, which is used to verify 
+        // r0 circuits such as the lift program.
         let verifying_key = ark_groth16::VerifyingKey::<Bn254>::deserialize_uncompressed(R0_SERIALIZED_UNCOMPRESSED_VK.as_slice())?;
+        
+        // Create the groups
         let g1 = G1::from_r0_bytes(&seal.a)?;
         let g1_c = G1::from_r0_bytes(&seal.c)?;
         let g2 = G2::from_r0_bytes(&seal.b)?;
+
+        // Create an ark proof from the R0 proof data
         let mut encoded_proof = Vec::new();
+
+        // The proof itself is not to be used for script-specific logic
+        // as such it should be serialized in its compressed form to save space. 
         let proof: Proof<ark_ec::bn::Bn<ark_bn254::Config>> = Proof::<Bn254> { a: g1.0, b: g2.0, c: g1_c.0 };
         proof.serialize_compressed(&mut encoded_proof)?;
-        // Serialize with serde_ark feature which under the hood is just
-        // uncompressed serialization.
-        // Re-serialize then deserialize to get the inner ark VK
+
+        // Serialize the verifying key in compressed form as well, to save space
+        // the verifying key here will never change, and as such it can be used
+        // for script specific logic.
         let mut serialized_vk = Vec::new();
         verifying_key.serialize_compressed(&mut serialized_vk).map_err(|_| R0Error::BincodeVkSerialization)?;
         let mut builder = ScriptBuilder::new();
-        builder.add_data(&id_bn254)?;
-        builder.add_data(&c1)?;
-        builder.add_data(&c0)?;
-        builder.add_data(&a1)?;
-        builder.add_data(&a0)?;
-        builder.add_i64(5)?;
-        builder.add_data(&encoded_proof)?;
-        builder.add_data(&serialized_vk)?;
-        builder.add_data(&[ZkTag::Groth16 as u8])?;
-        builder.add_op(OpZkPrecompile)?;
+
+        // push all public inputs to the script.
+        builder.add_data(&id_bn254)?; // control id
+        builder.add_data(&c1)?;  // claim digest hi
+        builder.add_data(&c0)?; // claim digest lo
+        builder.add_data(&a1)?; // control root hi
+        builder.add_data(&a0)?; // control root lo
+        builder.add_i64(5)?; // Inform how many public inputs there are, 
+        builder.add_data(&encoded_proof)?; // the proof itself, 
+        builder.add_data(&serialized_vk)?; // the verifying key
+        builder.add_data(&[ZkTag::Groth16 as u8])?; // the tag of which precompile.
+        builder.add_op(OpZkPrecompile)?; // precomp opcode
         Ok(builder)
     }
 }
-//    build_zk_script(&[seal, claim, hashfn, control_index, control_digests, journal, image_id, vec![stark_tag]]).unwrap()
--- crypto/txscript/src/zk_precompiles/risc0/zk_to_script/succinct.rs
+++ crypto/txscript/src/zk_precompiles/risc0/zk_to_script/succinct.rs
@@ -18,9 +18,19 @@ impl R0ScriptBuilder {
         journal: Digest,
         image_id: Digest,
     ) -> Result<ScriptBuilder> {
+        // Initialize script builder
         let mut builder = ScriptBuilder::new();
-        let serialized_claim: Digest = receipt.claim.clone().digest::<sha::Impl>();
-        builder.add_data(&serialized_claim.as_bytes().to_vec())?;
+
+        // The claim here might be already or not digested
+        // but in either case we need to extract the digest
+        // since that is what allows us to have a constant sized
+        // stark proof.
+        let serialized_claim: Digest = receipt.claim.digest::<sha::Impl>();
+        
+        builder.add_data(&serialized_claim.as_bytes())?;
+
+        // Extract the control index and control digests
+        // which are the merkle proof of inclusion.
         let (control_index, control_digests) = {
             (
                 receipt.control_inclusion_proof.index.to_le_bytes(),
@@ -29,12 +39,25 @@ impl R0ScriptBuilder {
         };
         builder.add_data(&control_index)?;
         builder.add_data(&control_digests)?;
+
+        // Add the seal but encode it as vec<u8>
         builder.add_data(&receipt.seal.iter().flat_map(|x| x.to_le_bytes()).collect::<Vec<u8>>())?;
+        
+        // Add the journal hash which contains the output of this
+        // program
         builder.add_data(journal.as_bytes())?;
+
+        // Add the image id which is the identifier of the program
         builder.add_data(image_id.as_bytes())?;
+
+        // Add the identifier of which r0 circuit was executed.
+        // Do note that the OpZkPrecompile R0 is expected to be used 
+        // with the lift program, other programs might overrun the mass limit.
         builder.add_data(&receipt.control_id.as_bytes().to_vec())?;
         builder.add_data(&[HashFnId::try_from(receipt.hashfn.as_str())? as u8])?;
-        builder.add_data(&[ZkTag::R0Succinct as u8].to_vec())?;
+
+        // This is an r0 succinct proof.
+        builder.add_data([ZkTag::R0Succinct as u8].as_ref())?;
         builder.add_op(OpZkPrecompile)?;
         Ok(builder)
     }
