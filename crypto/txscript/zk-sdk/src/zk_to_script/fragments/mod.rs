520b018495e4c7a216004120d17ee80be5e536e8
Ori Newman <orinewman1@gmail.com>
2026-06-28 10:55:36 +0300

    Rename append_r0_groth16_verifier -> append_r0_groth16_verifier_with_image_id and add append_r0_groth16_verifier (without image_id)

--- crypto/txscript/zk-sdk/src/zk_to_script/fragments/mod.rs
+++ crypto/txscript/zk-sdk/src/zk_to_script/fragments/mod.rs
@@ -2,6 +2,9 @@ mod groth16;
 mod prepare;
 mod succinct;
 
-pub use groth16::{append_r0_groth16_verifier, append_r0_groth16_verifier_with_fixed_journal, push_r0_groth16_proof};
+pub use groth16::{
+    append_r0_groth16_verifier, append_r0_groth16_verifier_dynamic_image_id, append_r0_groth16_verifier_with_fixed_journal,
+    push_r0_groth16_proof,
+};
 pub use prepare::{SuccinctWitnessBytes, prepare_r0_groth16_proof, prepare_r0_succinct_witness};
 pub use succinct::{append_r0_succinct_verifier, append_r0_succinct_verifier_with_fixed_journal, push_r0_succinct_witness};
